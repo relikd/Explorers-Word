7d256a5738003cd5e2bb34242cd6aeea2ea37332
Daniel Glück <hello@danielglueck.de>
2016-08-03 16:16:10 +0200

    ray casting sowie Anzeige von Buchstaben funktioniert bereits Austausch von Textur noch nicht ganz möglich

---
+++ Assets/LightSystem/VictorianLight.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class VictorianLight : MonoBehaviour 
+{
+	[HideInInspector] public bool Running = false;
+
+	private Material material;
+
+	VictorianLight() {
+		
+	}
+
+	void Update() {
+	}
+
+	void Start() {
+		enabled = true;
+		material = Resources.Load("Materials/Streetlight_On", typeof(Material)) as Material;
+	}
+
+	public void TurnLightsOn() {
+		GetComponent<Renderer> ().material = this.material;
+	
+	}
+	
+	void OnGUI ()
+	{
+		// Access InReach variable from raycasting script.
+		GameObject Player = GameObject.Find("FirstPersonCharacter");
+		TurnLightOn detection = Player.GetComponent<TurnLightOn>();
+
+		if (detection.InReach == true)
+		{
+			GUI.color = Color.white;
+			GUI.Box(new Rect(20, 20, 200, 25), "Press 'E' to open/close");
+		}
+	}
+}
