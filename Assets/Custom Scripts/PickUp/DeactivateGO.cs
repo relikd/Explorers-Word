60cf5e49be6b150626a531163e57467de8f685f1
Daniel Glück <hello@danielglueck.de>
2016-08-06 12:52:45 +0200

    holding objects works on static if set activate objects works

---
+++ Assets/Custom Scripts/PickUp/DeactivateGO.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeactivateGO : MonoBehaviour {
+
+	public string TriggerTag;
+	public bool shouldPickUpStatic = false;
+
+	void Start ()
+	{
+		
+	}
+		
+	void LateUpdate() {
+	
+		GameObject Player = GameObject.Find("FirstPersonCharacter");
+		Reachable detection = Player.GetComponent<Reachable>();
+
+		gameObject.isStatic = !shouldPickUpStatic;
+
+		if (Input.GetKeyUp (KeyCode.E) ) {
+			if (detection.RaycastHit.collider.tag == TriggerTag && detection.InReach == true) {
+				this.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	void OnGUI ()
+	{
+		// Access InReach variable from raycasting script.
+		GameObject Player = GameObject.Find("FirstPersonCharacter");
+		Reachable detection = Player.GetComponent<Reachable>();
+
+		if (detection.InReach == true)
+		{
+			GUI.color = Color.white;
+			GUI.Box(new Rect(Screen.width / 2, (Screen.height / 2) + 10, 200, 25), "Press 'E' to take");
+		}
+	}
+}
