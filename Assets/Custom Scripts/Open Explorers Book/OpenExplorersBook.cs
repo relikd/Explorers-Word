b341851f564fa23e98fa9af4ae73f7b56efe1fb9
Daniel Glück <hello@danielglueck.de>
2016-08-07 14:05:56 +0200

    Meetingprotokoll letzter Sitzung Gameobject buch wird jetzt durch Gui-Cammera über allem angezeigt

---
+++ Assets/Custom Scripts/Open Explorers Book/OpenExplorersBook.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ExplorersBook
+{
+	public class OpenExplorersBook : MonoBehaviour
+	{
+
+		void LateUpdate() {
+		
+			if (Input.GetKeyUp(KeyCode.B)) {
+				gameObject.SetActive (!gameObject.activeSelf);
+			}
+		}
+	}
+}
+
