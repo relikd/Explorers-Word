2c62a6ce58aa5d71ae54f93ad09c867038fffa3c
Daniel Glück <hello@danielglueck.de>
2016-08-07 22:58:55 +0200

    GUI Camera

---
+++ Assets/Custom Scripts/RotateObjects/RotateObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace AssemblyCSharp
+{
+	public class RotateObject : MonoBehaviour
+	{
+		public int degree; 
+
+		void LateUpdate() {
+			if (Input.GetKey (KeyCode.E)) {
+				gameObject.transform.Rotate (Vector3.up * degree);
+
+			}
+		}
+
+		void OnGUI ()
+		{
+			// Access InReach variable from raycasting script.
+			GameObject Player = GameObject.Find("FirstPersonCharacter");
+			Reachable detection = Player.GetComponent<Reachable>();
+
+			if (detection.InReach == true && detection.RaycastHit.collider.tag == gameObject.tag)
+			{
+				GUI.color = Color.white;
+				GUI.Box(new Rect(Screen.width / 2, (Screen.height / 2) + 10, 200, 25), "Press 'E' to rotate");
+			}
+		}
+	}
+}
+
