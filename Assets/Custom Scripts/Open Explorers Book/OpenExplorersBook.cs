5479c87e317d84b6b773d2786b7c8ff5a8884cb3
Daniel Glück <hello@danielglueck.de>
2016-08-09 13:13:14 +0200

    experimented with Text Transformation

--- Assets/Custom Scripts/Open Explorers Book/OpenExplorersBook.cs
+++ Assets/Custom Scripts/Open Explorers Book/OpenExplorersBook.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace ExplorersBook
 {
 	public class OpenExplorersBook : MonoBehaviour
 	{
-
+		public RectTransform TextPanel;
 		public GameObject explBook;
+		public RectTransform canvas;
+		public RectTransform UI_Element;
+		public Camera cammera;
+
 		private bool isActive = false;
+		private bool rotated = false;
+
 		void LateUpdate() {
 
 			if (Input.GetKeyUp(KeyCode.B)) {
-				//GameObject expBook = GameObject.Find ("ExplorersBook");
+				
 				isActive = !isActive;
 				explBook.SetActive (isActive);
 				DisablePlayerMovement ();
@@ -32,6 +39,34 @@ namespace ExplorersBook
 			Cursor.visible = isActive;
 			Cursor.lockState = CursorLockMode.Confined;
 			FirstPersonCharacter.GetComponent<MouseCrosshair> ().enabled = !isActive;
+		}
+
+		void OnGUI() {
+			//GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "Here is a block of text\nlalalala\nanother line\nI could do this all day!");
+			if (!rotated) { 
+				Vector3 explorersBookRotation = explBook.transform.rotation.eulerAngles;
+//				Vector3 explorersBookTranslation = explBook.transform.position;
+//				TextPanel.transform.position = explorersBookTranslation;
+//				TextPanel.transform.Rotate (explorersBookRotation);
+				UI_Element.transform.Rotate(explorersBookRotation);
+				RectTransform CanvasRect = canvas.GetComponent<RectTransform> ();
+
+				Vector2 ViewportPosition = cammera.WorldToViewportPoint (explBook.transform.position);
+				Vector2 WorldObject_ScreenPosition = new Vector2 (
+					((ViewportPosition.x * canvas.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+					((ViewportPosition.x * canvas.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+
+				Vector2 BookScreenCoord = RectTransformUtility.WorldToScreenPoint (cammera, explBook.transform.position);
+
+
+				UI_Element.anchoredPosition3D = WorldObject_ScreenPosition;
+				TextPanel.anchoredPosition = ViewportPosition;
+
+				//UI_Element.transform.SetParent(explBook.transform);
+
+				rotated = true;
+			}
 		}
 
 	}
