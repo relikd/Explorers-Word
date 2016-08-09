49cb006714387cf45ec6959775e165bcd1e4c364
Daniel Glück <hello@danielglueck.de>
2016-08-09 20:05:22 +0200

    Script for Deactivating Objects UserInput Field renamed one folder

---
+++ Assets/CustomScripts/RotateObjects/RotateObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+	public class RotateObject : MonoBehaviour
+	{
+		public int degree;
+		public bool rotateXAxis = false;
+		public bool rotateYAxis = true;
+		public bool rotateZAxis = false;
+
+		void LateUpdate() {
+			if (Input.GetKey (KeyCode.E)) {
+				gameObject.transform.Rotate(rotateXAxis?degree:0,rotateYAxis?degree:0,rotateZAxis?degree:0);
+
+			}
+		}
+
+		void OnGUI ()
+		{
+			// Access InReach variable from raycasting script.
+			GameObject Player = GameObject.Find("FirstPersonCharacter");
+			Reachable detection = Player.GetComponent<Reachable>();
+
+			if (detection.InReach == true && detection.RaycastHit.collider.tag == gameObject.tag)
+			{
+				GUI.color = Color.white;
+				GUI.Box(new Rect(Screen.width / 2, (Screen.height / 2) + 10, 200, 25), "Press 'E' to rotate");
+			}
+		}
+	}
+
+
---
+++ Assets/CustomScripts/UserInput/UserInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+using System.IO; 
+using AssemblyCSharp;
+
+
+
+ public class UserInput : MonoBehaviour {
+	public void handleUserInput(string UserInput) {
+		Debug.Log (UserInput);
+		GameObject gameObject = GameObject.Find (UserInput);
+		if (gameObject) {
+
+			Collider[] colliders = gameObject.GetComponentsInChildren<Collider> ();
+			foreach (Collider c in colliders) {
+				c.enabled = !c.enabled;
+			}
+			Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
+			foreach (Renderer r in renderers) {
+				r.enabled = !r.enabled;
+			}
+		}
+	}
+}
