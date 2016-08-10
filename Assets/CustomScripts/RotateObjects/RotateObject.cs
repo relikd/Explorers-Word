d81598a411ae610d05f38055828d2afaba079fdf
Daniel Glück <hello@danielglueck.de>
2016-08-10 14:58:10 +0200

    @oleg noch nicht sauber machen using 1 intractable interface now deleted old custom Scripts folder copied rotateObject usw. to new one

--- Assets/CustomScripts/RotateObjects/RotateObject.cs
+++ Assets/CustomScripts/RotateObjects/RotateObject.cs
@@ -1,34 +1,57 @@
-﻿//using UnityEngine;
-//using UnityEditor;
-//using System.Collections;
-//
-//
-//	public class RotateObject : MonoBehaviour
-//	{
-//		public int degree;
-//		public bool rotateXAxis = false;
-//		public bool rotateYAxis = true;
-//		public bool rotateZAxis = false;
-//
-//		void LateUpdate() {
-//			if (Input.GetKey (KeyCode.E)) {
-//				gameObject.transform.Rotate(rotateXAxis?degree:0,rotateYAxis?degree:0,rotateZAxis?degree:0);
-//
-//			}
-//		}
-//
-//		void OnGUI ()
-//		{
-//			// Access InReach variable from raycasting script.
-//			GameObject Player = GameObject.Find("FirstPersonCharacter");
-//			Reachable detection = Player.GetComponent<Reachable>();
-//
-//			if (detection.InReach == true && detection.RaycastHit.collider.tag == gameObject.tag)
-//			{
-//				GUI.color = Color.white;
-//				GUI.Box(new Rect(Screen.width / 2, (Screen.height / 2) + 10, 200, 25), "Press 'E' to rotate");
-//			}
-//		}
-//	}
-//
-//
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Interaction
+{
+	public class RotateObject : MonoBehaviour, Interactable
+	{
+		private RotationLimiter rl_script;
+
+		public float rotateXAxisBy = 0;
+		public float rotateYAxisBy = 10;
+		public float rotateZAxisBy = 0;
+		public bool stepAngle = false;
+		[HideInInspector] bool shouldDepictText;
+
+		void LateUpdate() {
+			
+		}
+
+		public void HandleRaycastCollission(){
+		if (stepAngle ? Input.GetKeyUp(KeyCode.R) : Input.GetKey(KeyCode.R)) {
+				gameObject.transform.Rotate(rotateXAxisBy,rotateYAxisBy,rotateZAxisBy);
+
+				if (rl_script) {
+					int ool = rl_script.outOfLimit ();
+					if ((ool & 1)!=0) rotateXAxisBy *= -1;
+					if ((ool & 2)!=0) rotateYAxisBy *= -1;
+					if ((ool & 4)!=0) rotateZAxisBy *= -1;
+
+					if (ool > 0 && !rl_script.isDirectlyOnLimit())
+						gameObject.transform.Rotate(rotateXAxisBy,rotateYAxisBy,rotateZAxisBy);
+				}
+				shouldDepictText = false;
+		}
+		}
+
+		public void EnableGUI(bool enable) {
+			shouldDepictText = enable;
+
+		}
+			
+		void Start () {
+			rl_script = gameObject.GetComponent<RotationLimiter>();
+		}
+
+		void OnGUI ()
+		{
+			if (shouldDepictText)
+			{
+				GUI.color = Color.white;
+				GUI.Box(new Rect(Screen.width / 2, (Screen.height / 2) + 10, 200, 25), "Press 'R' to rotate");
+			}
+		}
+	}
+}
+
