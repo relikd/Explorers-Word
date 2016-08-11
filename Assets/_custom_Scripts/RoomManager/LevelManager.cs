a09cae3201e5b87926709ad8e3a3c1874d54b47a
relikd <info@relikd.de>
2016-08-11 18:46:57 +0200

    Level Manager and Room 1 Manager. Fixed bugs in Rotation Limiter

---
+++ Assets/_custom_Scripts/RoomManager/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	static LevelManager Instance;
+
+	void Start () {
+		if (Instance != null)
+			GameObject.Destroy (gameObject);
+		else {
+			GameObject.DontDestroyOnLoad (gameObject);
+			Instance = this;
+		}
+	}
+
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Alpha1)) {
+			SceneManager.LoadScene ("room_0");
+		} else if (Input.GetKeyUp (KeyCode.Alpha2)) {
+			SceneManager.LoadScene ("room_1");
+		}
+	}
+}
