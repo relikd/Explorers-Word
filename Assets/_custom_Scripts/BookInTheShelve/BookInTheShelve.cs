9965e1a650bd3715ae38fc6337eb13890e43c960
Daniel Glück <hello@danielglueck.de>
2016-08-11 13:38:50 +0200

    Script für buch im regal

---
+++ Assets/_custom_Scripts/BookInTheShelve/BookInTheShelve.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Interaction;
+
+public class BookInTheShelve : MonoBehaviour, Interactable 
+{
+	private bool shouldDisplayText;
+
+	public void HandleRaycastCollission() {
+		if (Input.GetKeyUp (KeyCode.E)) {
+			//DO STUFF
+			Debug.Log("IsTriggered");
+		}
+	}
+
+	public void EnableGUI(bool enable) {
+		shouldDisplayText = enable;
+	}
+
+	void OnGUI() {
+		if (shouldDisplayText)
+		{
+			GUI.color = Color.white;
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 200, 25), "Press 'E' to pull");
+		}
+	}
+}
+
