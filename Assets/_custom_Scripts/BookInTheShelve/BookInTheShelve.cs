ef551de558860e4555c4971b4ad560fb6e1086ab
Daniel Glück <hello@danielglueck.de>
2016-08-11 15:28:25 +0200

    short Animation for BookInThe Shelve

--- Assets/_custom_Scripts/BookInTheShelve/BookInTheShelve.cs
+++ Assets/_custom_Scripts/BookInTheShelve/BookInTheShelve.cs
@@ -3,15 +3,24 @@ using System.Collections;
 using UnityEngine;
 using Interaction;
 
+
 public class BookInTheShelve : MonoBehaviour, Interactable 
-{
+{	
+	
+	
 	private bool shouldDisplayText;
 
 	public void HandleRaycastCollission() {
 		if (Input.GetKeyUp (KeyCode.E)) {
 			//DO STUFF
-			Debug.Log("IsTriggered");
+			playAnimation ();
+
 		}
+	}
+
+	private void playAnimation() {
+		Animator anim = gameObject.GetComponent<Animator> ();
+		anim.SetBool ("Trigger", true);
 	}
 
 	public void EnableGUI(bool enable) {
