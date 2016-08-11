ea97af2a883e7ba86e0b51eba35c9fe6430f3106
Daniel Glück <hello@danielglueck.de>
2016-08-11 17:38:33 +0200

    ScriptForLoadingNext Scene

---
+++ Assets/_custom_Scripts/LoadScenes/LoadNextScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 positionPlayer = gameObject.transform.position;
+		if (positionPlayer.z > 10) {
+			SceneManager.LoadScene ("room_0");
+		}	
+	}
+}
