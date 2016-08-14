cca88b1774c44c969661a95756a7772bc503b369
relikd <info@relikd.de>
2016-08-14 20:54:35 +0200

    nicely animated book shelve

--- Assets/_custom_Scripts/Interaction/Triggered Interaction/Puzzle1BookHandler.cs
+++ Assets/_custom_Scripts/Interaction/Triggered Interaction/Puzzle1BookHandler.cs
@@ -8,27 +8,35 @@ public class Puzzle1BookHandler : TriggerInteractionCallback
 	
 	override public void OnTriggerInteraction (TriggerInteraction sender) {
 		if (sender.triggerActive) {
-			playAnimation ();
-			// TODO: wait till animation finished
-			shelfOpen = true;
 			sender.responseMessage = "A rusty mechanism moved the bookshelf";
+			StartCoroutine (playAnimation ());
 		}
 	}
 
-	private void playAnimation() {
+	IEnumerator playAnimation() {
 		Animator anim = gameObject.GetComponent<Animator> ();
 		if (anim) {
 			anim.SetBool ("Trigger", true);
+			yield return new WaitForSeconds (getAnimationDuration (anim, "BookInTheShelveAnimation"));
+			//anim.SetBool ("Trigger", false); // dont reset. should be animated once
 		}
+		shelfOpen = true;
 	}
 
 	void Update() {
 		if (shelfOpen && shelfRotationAngle > -17.0f) {
+			// TODO: play sound 'stone on stone scrape'
 			GameObject bsrc = GameObject.Find ("BookShelfRotationContainer");
 			if (bsrc) {
-				shelfRotationAngle -= 10.0f * Time.deltaTime;
+				shelfRotationAngle -= 15.0f * Time.deltaTime;
 				bsrc.transform.eulerAngles = new Vector3 (0, shelfRotationAngle, 0);
 			}
 		}
+	}
+
+	private float getAnimationDuration(Animator a, string n) {
+		foreach (AnimationClip clip in a.runtimeAnimatorController.animationClips)
+			if (clip.name == n) return clip.length;
+		return 0;
 	}
 }
