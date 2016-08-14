4b1cf30b978b543d37830931eb597e5d12b3c5c9
relikd <info@relikd.de>
2016-08-14 18:11:55 +0200

    Triggered Interaction

---
+++ Assets/_custom_Scripts/Interaction/Triggered Interaction/Puzzle1BookHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle1BookHandler : InteractionTrigger
+{
+	private bool shelfOpen = false;
+	private float shelfRotationAngle = 0.0f;
+	
+	override public void OnTriggerInteraction (TriggerInteractable sender) {
+		if (sender.triggerActive) {
+			playAnimation ();
+			// TODO: wait till animation finished
+			shelfOpen = true;
+			sender.responseMessage = "A rusty mechanism moved the bookshelf";
+		}
+	}
+
+	private void playAnimation() {
+		Animator anim = gameObject.GetComponent<Animator> ();
+		if (anim) {
+			anim.SetBool ("Trigger", true);
+		}
+	}
+
+	void Update() {
+		if (shelfOpen && shelfRotationAngle > -17.0f) {
+			GameObject bsrc = GameObject.Find ("BookShelfRotationContainer");
+			if (bsrc) {
+				shelfRotationAngle -= 10.0f * Time.deltaTime;
+				bsrc.transform.eulerAngles = new Vector3 (0, shelfRotationAngle, 0);
+			}
+		}
+	}
+}
