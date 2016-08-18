c6d2a6482c2ba6e22b10a242303c1e639aa9660a
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-18 20:21:42 +0200

    BITTE LOGGEN JUNGS! LogWriter.WriteLog ist jetzt überladen und kann auch mit (String, GameObject) aufgerufen werden. Versucht immer diesen Aufruf zu verwenden, außer natürlich euer Skript ist statisch etc.
    Define oben in LogWriter nach Bedarf einschalten um Loggen zu aktivieren, Sollte nicht immer an sein, erstellst sonst beim testen immer einen Log :-P

--- Assets/_custom_Scripts/PlayerLog/LogWriter.cs
+++ Assets/_custom_Scripts/PlayerLog/LogWriter.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 using System.Collections;
 using System;
 using System.IO;
+using System.Reflection;
+using System.Diagnostics;
 
- 
 public static class LogWriter  {
     static string dir = @"Logs";
     private static string path = @"Logs/GameLog "+ DateTime.Now.ToString("yyyy-MM-dd")+" "+ DateTime.Now.ToString("HH-mm-ss") + ".txt";
@@ -18,7 +19,7 @@ public static class LogWriter  {
         
     }
 
-    public static void WriteLog(String line)
+    public static void WriteLog(String line, GameObject callingObj)
     // schreibt eine Zeile ins Logfile, erstellt falls nötig das File und das directory
     {
 
@@ -35,9 +36,40 @@ public static class LogWriter  {
         }
         using (StreamWriter sw = File.AppendText(path))
         {
-            sw.WriteLine(line);
+            StackTrace stackTrace = new StackTrace();
+            MethodBase methodBase = stackTrace.GetFrame(1).GetMethod();
+            String CallingObjName = callingObj.name;
+            string methodName = methodBase.Name;
+            string typeName = methodBase.DeclaringType.Name;
+            sw.WriteLine(DateTime.Now.ToString("HH:mm:ss: ") + " ; " + CallingObjName + " ; " + typeName + " ; " + methodName + " ; " + line);
         }
      #endif
+
+    }
+    public static void WriteLog(String line)
+    // schreibt eine Zeile ins Logfile, erstellt falls nötig das File und das directory
+    {
+
+#if (LOG)
+        if (newgame)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            //Fügt eine Zeile mit der Startuhrzeit ein
+            using (StreamWriter sw = File.CreateText(path)) sw.WriteLine(DateTime.Now.ToString("HH:mm:ss") + ":");
+            newgame = false;
+        }
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase methodBase = stackTrace.GetFrame(1).GetMethod();
+            string methodName = methodBase.Name;
+            string typeName = methodBase.DeclaringType.Name;
+            sw.WriteLine(DateTime.Now.ToString("HH:mm:ss: ") + " ; " + "Kein Objektname gegeben" + " ; " + typeName + " ; " + methodName + " ; " + line);
+        }
+#endif
 
     }
 }
