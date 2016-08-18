afa3ebc87a7626730f9d067bad169991419a5aeb
Daniel Glück <hello@danielglueck.de>
2016-08-18 17:18:15 +0200

    current version of UserInput Script NOT FINISHED!

--- Assets/_custom_Scripts/GUIManager/UserInput.cs
+++ Assets/_custom_Scripts/GUIManager/UserInput.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using System.Text;
 using System.IO; 
@@ -9,10 +10,16 @@ using System.IO;
  public class UserInput : MonoBehaviour {
 
 	public string TriggerTag;
+	Queue visibleObjects = new Queue();
+	private int QueueLength = 3;
+
+	LinkedList<GameObject> visibleObjects2 = new LinkedList<GameObject>();
 
 	void Start() {
 		
 	}
+
+
 
 	public void handleUserInput(string UserInput) {
 		if (UserInput == "all") {
@@ -23,9 +30,28 @@ using System.IO;
 //		if (gameObject) {
 //			deactivateObject (gameObject);
 //		}
-
-		disableGameObjectViaTag (UserInput);
+	
+		if (!handleGameObjectInQueue (UserInput)) {
+			disableGameObjectViaTag (UserInput);
+		}
 		ResetInputField();
+	}
+
+	private void updateQueueWithGameObject(GameObject go) {
+		if (visibleObjects2.Count == QueueLength - 1) {
+			
+			deactivateObject (visibleObjects2.First.Value, false);
+			visibleObjects2.RemoveFirst();
+			visibleObjects2.AddLast (go);
+		} else {
+			visibleObjects2.AddLast (go);
+		}
+	}
+
+	private void deactiveQueueObjects() {
+		foreach (GameObject go in visibleObjects2) {
+			deactivateObject (go, true);
+		}
 	}
 
 	private void ResetInputField() {
@@ -36,38 +62,51 @@ using System.IO;
 		}
 	}
 
-	private void deactivateObject(GameObject go) {
+	private void deactivateObject(GameObject go, bool deactivate) {
 		Collider[] colliders = go.GetComponentsInChildren<Collider> ();
 		foreach (Collider c in colliders) {
-			c.enabled = !c.enabled;
+			c.enabled = !deactivate;
 		}
 		Renderer[] renderers = go.GetComponentsInChildren<Renderer>();
 		foreach (Renderer r in renderers) {
-			r.enabled = !r.enabled;
+			r.enabled = !deactivate;
 		}
 		Rigidbody rigidbody = go.GetComponent<Rigidbody> ();
 		if (rigidbody) {
-			rigidbody.useGravity = !rigidbody.useGravity;
+			rigidbody.useGravity = !deactivate;
 		}
 
 	}
 
 	public void deactivateAllGameObjects() {
 		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(TriggerTag);
-
 		foreach (GameObject go in gameObjects) {
 			if (go.activeInHierarchy) {
-				deactivateObject (go);
+				deactivateObject (go, true);
 				go.transform.hasChanged = !go.transform.hasChanged;
 			}
 		}
+	}
+
+	private bool handleGameObjectInQueue(string text)
+	{
+		bool result = false;
+		foreach (GameObject go in visibleObjects2) {
+			if (go.name == text) {
+				deactivateObject (go, false);
+				visibleObjects2.Remove (go);
+				result = true;
+			}
+		}
+		return result;
 	}
 
 	private void disableGameObjectViaTag(string text) {
 		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag (TriggerTag);
 		foreach (GameObject go in gameObjects) {
 			if (go.activeInHierarchy && go.name == text) {
-				deactivateObject (go);
+				updateQueueWithGameObject (go);
+				deactiveQueueObjects();
 				go.transform.hasChanged = !go.transform.hasChanged;
 			}
 		}
