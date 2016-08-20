7e22eba7efe9a5b4d989814edfc1a2c0826607f5
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-20 23:18:26 +0200

    Raum 2 Rätsel begonnen, Objekt einsammeln implementiert, Zerschneiden von Bild implementiert. Resources Ordner erstellt: Hier kommt alles rein, was im Code mit Resources.Load aufgerufen werden muss.

---
+++ Assets/_custom_Scripts/Interaction/CutInteraction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Interaction;
+
+/**
+ * Ein Skript, dass die Interaktion mit dem Bild in Raum 2 erlaubt, um es zu zerschneiden. Dabei wird das Material ersetzt. Die Interaktion kann nur einmal erfolgen.
+ */
+public class CutInteraction : Interactable
+{
+    
+    private string actionMessage = "cut open the painting";
+    private Material newMaterial = (Material)Resources.Load("chineseBattleFieldPaintingMaterialRipped", typeof(Material));
+
+    
+    /**
+     * Gibt den Text zurueck, der dem Spieler angezeigt wird, wenn er mit der Maus über das interagierbare Objekt faehrt.
+     */ 
+    override public string interactMessage()
+    {
+        return actionMessage;
+    }
+
+    /**
+     * Ersetzt das Material des Bilds und zerstört den BoxCollider des Gameobjects um eine erneute Interaktion zu verhinden.
+     */
+    override public void OnInteractionKeyPressed()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = newMaterial;
+        Destroy(gameObject.GetComponent<BoxCollider>());
+        this.enabled = false;
+    }
+}
