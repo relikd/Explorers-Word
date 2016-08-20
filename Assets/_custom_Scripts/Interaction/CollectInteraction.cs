080ef81e41c825e45e1df59b7c249b9ee0e87464
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-20 23:18:26 +0200

    Raum 2 Rätsel begonnen, Objekt einsammeln implementiert, Zerschneiden von Bild implementiert. Resources Ordner erstellt: Hier kommt alles rein, was im Code mit Resources.Load aufgerufen werden muss.

--- Assets/_custom_Scripts/Interaction/CollectInteraction.cs
+++ Assets/_custom_Scripts/Interaction/CollectInteraction.cs
@@ -1,14 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 using Interaction;
-
+/**
+ * Ein Skript, das es erlaubt GameObjects einzusammeln. Da es kein Inventar gibt wird das GO nicht eingesammelt, sondern deaktiviert. Zusaetlich wir ein beliebiges Skript an ein anderes GO angehaengt um die Funktionalitaet zur Vefuegung zu stellen, die durch das Einsammeln möglich wird. */
 public class CollectInteraction : Interactable
 {
-	override public string interactMessage() {
-		return "take";
+    [SerializeField] private string NameOfItem;
+    [SerializeField] private String NameOfNewScript;
+    [SerializeField] private GameObject TargetGameObject;
+
+    /**
+     * Gibt den Text zurueck, der dem Spieler angezeigt wird, wenn er mit der Maus über das interagierbare Objekt faehrt.
+     */
+    override public string interactMessage() {
+		return "pick up " + NameOfItem ;
 	}
 
-	override public void OnInteractionKeyPressed () {
+    /**
+     * Fuegt dem uebergebenen GameObject das uebergebene Skript an, falls vorhanden.
+     */
+    private void attachScript()
+    {
+        TargetGameObject.AddComponent(Type.GetType(NameOfNewScript));
+    }
+
+    /**
+     * Ruft attachScript auf und deaktiviert das eingesammelte GameObject.
+     */
+    override public void OnInteractionKeyPressed () {
+        if (TargetGameObject)  attachScript();
 		this.gameObject.SetActive(false);
 	}
 }
