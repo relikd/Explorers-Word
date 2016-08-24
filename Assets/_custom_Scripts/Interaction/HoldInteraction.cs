b7419efec5585b87d86c091a60a9cd69bd45539a
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-25 00:47:30 +0200

    iwie spinnt mein git, sollte der aktuelle stand sein, zur sicherhheit, muss aber noch weiter gedebuggt werden

--- Assets/_custom_Scripts/Interaction/HoldInteraction.cs
+++ Assets/_custom_Scripts/Interaction/HoldInteraction.cs
@@ -1,40 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Interaction;
+using System;
 
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+/**
+ * Ein Skript, welches es ermoeglicht, Objekte zu tragen. Es verhindert uebermaessiges clipping, gestattet jedoch dem getragenen Objekt ohne Kollision an Ecken vorbeibewegt zu werden.
+ */
 public class HoldInteraction : Interactable
 {
-	public GameObject target;
+    private float distance = 2.5f;
+    private float smooth = 100;
 
-	override public string interactMessage() {
-		return "hold";
-	}
+    private DummyColliderObject DummyObj;
+    private BoxCollider DummyCollider;
+    Rigidbody dummyRigid;
 
-	override public void OnInteractionKeyPressed() {
-		HandleRigidBody (true);
-		Pickup ();
-	}
+    private BoxCollider myCollider;
+    private Rigidbody myRigid;
+    
 
-	void Update() {
-		if (Input.GetKeyDown (theKeyCode())) {
-			Drop ();
-			HandleRigidBody (false);
-		}
-	}
+    /**
+     * Initialisiert die Variablen des Objekts und des DummyObjekts.
+     */
+    void Awake()
+    {
+        myCollider = gameObject.GetComponent<BoxCollider>();
+        myRigid = gameObject.GetComponent<Rigidbody>();
+        GameObject Dummy = new GameObject();
+        DummyObj = Dummy.AddComponent<DummyColliderObject>();
+        DummyObj.distance = distance;
+        DummyObj.smooth = smooth;
+        DummyObj.safe = gameObject.transform.position;
+        DummyObj.saferotate = gameObject.transform.rotation;
+        dummyRigid = Dummy.AddComponent<Rigidbody>();
+        dummyRigid.useGravity = false;
+        dummyRigid.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        dummyRigid.isKinematic = true;
+        DummyCollider = Dummy.AddComponent<BoxCollider>();
+        DummyCollider.center = myCollider.center;
+        DummyCollider.size = myCollider.size;
+    }
+    /**
+    * Aktualisiert die Position des getragenen Objekts auf eine mit Lerp interpolierte Position zwischen der aktuellen Lage und einem sicheren Zielpunkt.Erlaubt auch das Fallenlassen des Objekts.
+    */
+    void FixedUpdate()
+    {
+        if (DummyObj.carrying)
+        {
+            if (Input.GetKeyUp(theKeyCode())) drop();
 
-	void Pickup() {
-		if (target)
-			this.transform.position = this.target.transform.position;
-		this.transform.parent = GameObject.Find ("FirstPersonCharacter").transform;
-	}
+            Vector3 NewLoc = Vector3.Lerp(gameObject.transform.position, DummyObj.safe, Time.deltaTime * 12);
+            gameObject.transform.position = NewLoc;
+            gameObject.transform.rotation = DummyObj.saferotate;//Quaternion.identity;
+          
+        }
+    }
+    public override string interactMessage()
+    {
+        return "carry";
+    }
 
-	void Drop() {
-		this.transform.parent = null;
-	}
-
-	private void HandleRigidBody(bool isKinematic) {
-		if (gameObject.GetComponent<Rigidbody> ()) {
-			gameObject.GetComponent<Rigidbody> ().isKinematic = isKinematic;
-		}
-	}
+    public override void OnInteractionKeyPressed()
+    {
+    //Muss implementiert sein, es soll jedoch eine andere Klick-Logk verwendet werden.
+    }
+    /**
+     * Hebt beim druecken des Knopfes das Objekt auf.
+     */
+    override public void OnInteractionKeyDown()
+    {
+        if (!DummyObj.carrying)
+        {
+            pickup();
+        }
+    }
+    /**
+     * Schaltet die Werte zurecht um das Tragen des Objekts zu ermoeglichen. Aktiviert die Interaktion mit dem Dummy-Objekt. Deaktiviert die Interaktionmit dem echten Objekt.
+     */
+     private void pickup()
+    {
+        LogWriter.WriteLog("aufgehoben", gameObject);
+        myRigid.isKinematic = true;
+        myRigid.useGravity = false;
+        DummyCollider.enabled = true;
+        myCollider.enabled = false;
+        dummyRigid.isKinematic = false;
+       // DummyObj.safe = gameObject.transform.position;
+        DummyObj.carrying = true;
+    }
+    /**
+     * Schaltet die Werte zurecht um das normale Verhalten des Objekts wiederherzustellen. Deaktiviert die Interaktionmit dem Dummy-Objekt. Aktiviert die Interaktion mit dem echten Objekt.
+     */
+    private void drop()
+    {
+        LogWriter.WriteLog("fallen gelassen", gameObject);
+        dummyRigid.isKinematic = true;
+        myRigid.useGravity = true;
+        myRigid.isKinematic = false;
+        DummyCollider.enabled = false;
+        myCollider.enabled = true;
+        DummyObj.carrying = false;       
+    } 
 }
