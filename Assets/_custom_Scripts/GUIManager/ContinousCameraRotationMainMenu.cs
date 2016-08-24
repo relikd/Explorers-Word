0f7f4d4416ff5343792ee4535daae792ccbe8695
Daniel Mügge <hello@daniel-muegge.de>
2016-08-24 03:36:26 +0200

    StartMenuScene implemented. Fully functional (Quit Application Button only works when project is builded as an executable file <-- NO BUG!)

---
+++ Assets/_custom_Scripts/GUIManager/ContinousCameraRotationMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinousCameraRotationMainMenu : MonoBehaviour {
+
+	private Transform cameraTransform;
+	// Use this for initialization
+	void Start () {
+		cameraTransform =  this.gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cameraTransform.Rotate (new Vector3 (0, 0.1f, 0));
+	}
+}
---
+++ Assets/_custom_Scripts/RoomManager/MainMenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Button startGameButton;
+	public Button exitGameButton;
+	public Canvas quitMenuPopUp;	
+
+
+	private GlobalSoundPlayer globalSoundPlayer;
+	// Use this for initialization
+	void Start () {
+		startGameButton = startGameButton.GetComponent<Button>();
+		exitGameButton = exitGameButton.GetComponent < Button> ();
+		quitMenuPopUp = quitMenuPopUp.GetComponent<Canvas> ();
+		quitMenuPopUp.enabled = false;
+		globalSoundPlayer = this.gameObject.GetComponent<GlobalSoundPlayer> ();
+		globalSoundPlayer.StartAudio ();
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene ("room_0", LoadSceneMode.Single);
+	}
+
+	public void ExitGameCanceledPressed(){
+		quitMenuPopUp.enabled = false;
+		startGameButton.enabled = true;
+		exitGameButton.enabled = true;
+	}
+
+	public void ExitGamePressed(){
+		quitMenuPopUp.enabled = true;
+		startGameButton.enabled = false;
+		exitGameButton.enabled = false;
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+}
