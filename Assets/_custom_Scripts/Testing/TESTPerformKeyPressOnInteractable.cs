d96d8e77fc8162bfaeace0c1a77b7ca84237a34e
relikd <info@relikd.de>
2016-08-25 19:33:07 +0200

    Toggle Interaction for things like, open door if key collected
    Usage:
    1) you add two scripts to the door
    - plain interaction "cannot open door" and
    - triggered interaction "[leave_room]"
    2) set the triggered interaction to enabled = false
    3) add toggle interaction to the keys
    4) assign both scripts from door to the keys' toggle interaction

---
+++ Assets/_custom_Scripts/Testing/TESTPerformKeyPressOnInteractable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Interaction;
+
+namespace XplrTest {
+	public class TESTPerformKeyPressOnInteractable : MonoBehaviour {
+		void Awake () {
+			Interactable[] scripts = GetComponents <Interactable>();
+			foreach (Interactable scpt in scripts) {
+				scpt.OnInteractionKeyPressed ();
+			}
+		}
+	}
+}
