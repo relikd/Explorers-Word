4684dc1fae99c810a6e944ba6a62fe3be722aa85
Oleg Geier <oleg@relikd.de>
2016-08-25 19:33:07 +0200

    Toggle Interaction for things like, open door if key collected
    Usage:
    1) you add two scripts to the door
    - plain interaction "cannot open door" and
    - triggered interaction "[leave_room]"
    2) set the triggered interaction to enabled = false
    3) add toggle interaction to the keys
    4) assign both scripts from door to the keys' toggle interaction

---
+++ Assets/_custom_Scripts/Interaction/ToggleInteraction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+
+namespace Interaction
+{
+	/**
+	 * Toggles the assigned scripts active property
+	 */
+	public class ToggleInteraction : PlainInteraction {
+		/** Possible interaction outcome */
+		[Serializable]
+		enum OnInteractionOperation {
+			DoNothing,
+			DeleteGameObject,
+			CollectGameObject
+		}
+		/** Defines what should happen after an interaction was triggered */
+		[SerializeField] private OnInteractionOperation onInteraction;
+		/** The list of scripts which should be toggled */
+		[Tooltip("for single usage: add this script to the list below")]
+		[SerializeField] private Interactable[] toggleScriptsEnabledState;
+
+		/**
+		 * Toggles all assigned interaction scripts and displays a message (eventually delete object)
+		 */
+		override public void OnInteractionKeyPressed() {
+			centeredMessage (responseMessage);
+
+			foreach (Interactable script in toggleScriptsEnabledState)
+				script.interactionEnabled = !script.interactionEnabled;
+
+			switch (onInteraction) {
+			case OnInteractionOperation.CollectGameObject:
+			case OnInteractionOperation.DeleteGameObject:
+				Destroy (gameObject);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
