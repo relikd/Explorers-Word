d0991a46bc0bbc56f9d4d952261646d537aff004
relikd <info@relikd.de>
2016-08-25 22:56:08 +0200

    Toggle Interaction extension fix bug related obsolete depricated

--- Assets/_custom_Scripts/Interaction/ToggleInteraction.cs
+++ Assets/_custom_Scripts/Interaction/ToggleInteraction.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections;
 
 namespace Interaction
 {
@@ -11,6 +12,7 @@ namespace Interaction
 		[Serializable]
 		enum OnInteractionOperation {
 			DoNothing,
+			DeactivateThisScript,
 			DeleteGameObject,
 			CollectGameObject
 		}
@@ -21,20 +23,35 @@ namespace Interaction
 		[SerializeField] private Interactable[] toggleScriptsEnabledState;
 
 		/**
-		 * Toggles all assigned interaction scripts and displays a message (eventually delete object)
+		 * Display message and run Coroutine {@link #toggleScriptsAfter}
 		 */
 		override public void OnInteractionKeyPressed() {
 			centeredMessage (responseMessage);
+			StartCoroutine (toggleScriptsAfter());
+		}
+		/**
+		 * Run the script toggle after 0.01 seconds so that it will not trigger {@link Interactable#OnInteractionKeyPressed()}
+		 * Then evaluate any changes for this GameObject to be made (delete, collect, ...)
+		 */
+		IEnumerator toggleScriptsAfter() {
+			// wait to suppress multiple interaction on same gaming object
+			yield return new WaitForSeconds (0.01f);
 
-			foreach (Interactable script in toggleScriptsEnabledState)
-				script.interactionEnabled = !script.interactionEnabled;
-
+			foreach (Interactable script in toggleScriptsEnabledState) {
+				if (script)
+					script.interactionEnabled = !script.interactionEnabled;
+			}
+			// have to deactivate the old message in case it will be deactivated or deleted
+			// but will be set on true anyway if it is still in reach
+			EnableGUI (false);
+			// what happens after the toggle
 			switch (onInteraction) {
 			case OnInteractionOperation.CollectGameObject:
 			case OnInteractionOperation.DeleteGameObject:
 				Destroy (gameObject);
 				break;
-			default:
+			case OnInteractionOperation.DeactivateThisScript:
+				this.interactionEnabled = false;
 				break;
 			}
 		}
