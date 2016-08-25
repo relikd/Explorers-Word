5c4f299fe2e979a6dfc56e252e3a43b154cf7cf7
Oleg Geier <oleg@relikd.de>
2016-08-26 00:05:32 +0200

    cool new Reachable script. Reach will increase if you look to the bottom

--- Assets/_custom_Scripts/Detection/Reachable.cs
+++ Assets/_custom_Scripts/Detection/Reachable.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using Interaction;
+using System;
 
 /**
  * Search for objects which are in reach of the player
  */
 public class Reachable : MonoBehaviour
 {
-	GameObject currentGameObject;
-	bool initial = true;
+	private GameObject currentGameObject;
 	public float Reach = 0F;
+	public float proceduralReach;
 
 	// PRIVATE SETTINGS
 	[HideInInspector] public RaycastHit RaycastHit;
@@ -23,21 +24,13 @@ public class Reachable : MonoBehaviour
 		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0F));
 		RaycastHit hit; // Variable reading information about the collider hit.
 
+		proceduralReach = getDownLookingReach (1.9f, 50, 10);
 		// Cast a ray from the center of screen towards where the player is looking.
-		if (Physics.Raycast (ray, out hit, Reach)) {
+		if (Physics.Raycast (ray, out hit, proceduralReach)) {
 			RaycastHit = hit;
 
-
 			GameObject go = hit.transform.gameObject;
-
-			if (initial) {
-				currentGameObject = go;
-				initial = false;
-			}
-
-			if (currentGameObject == go ) {
-				currentGameObject = go;
-			} else {
+			if (currentGameObject != go ) {
 				deactiveGUI();
 				activateCrosshair (false);
 				currentGameObject = go;
@@ -54,7 +47,24 @@ public class Reachable : MonoBehaviour
 		} else {
 			deactiveGUI ();
 			activateCrosshair (false);
+			currentGameObject = null;
 		}
+	}
+	/**
+	 * Gradually increas reach if player is looking down to the floor
+	 * @param maxReach will be used if looking down, should be as high as the player/camera (1.9f)
+	 * @param startingAngle from which point on the increase should start. 0 - 90, where 90 is down and 0 horizontal
+	 * @param graduallyOverDegrees dont jump from Reach to maxReach, increase partly linear
+	 * @return the new reach distance
+	 */
+	private float getDownLookingReach(float maxReach, float startingAngle, float graduallyOverDegrees) {
+		float currentAngle = Camera.main.transform.eulerAngles.x;
+		if (currentAngle > startingAngle && currentAngle <= 90) {
+			float tmp = Camera.main.transform.eulerAngles.x - startingAngle;
+			// get reach from settings and add enough to get up to at least 1.9 (the height of the player)
+			return Reach + Mathf.Min (tmp / graduallyOverDegrees, 1.0F) * Mathf.Max(maxReach-Reach, 0);
+		}
+		return Reach;
 	}
 	/**
 	 * Update Cursor if interaction is possible
