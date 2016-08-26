df704b43ca221d49281376b5a88644b5c873bb48
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-26 04:23:43 +0200

    Tragen jetzt möglich:
    Pivotpunkt des Objekts muss mittig sein / es muss entsprechend in einem Parent objekt platziert sein.
    
    genaue Erklärung im nächsten Daily
    
    + Protokoll von heute

--- Assets/_custom_Scripts/Interaction/HoldInteraction.cs
+++ Assets/_custom_Scripts/Interaction/HoldInteraction.cs
@@ -1,128 +1,185 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
+
 
 namespace Interaction
 {
-	[RequireComponent(typeof(BoxCollider))]
-	[RequireComponent(typeof(Rigidbody))]
-	/**
-	 * Ein Skript, welches es ermoeglicht, Objekte zu tragen. Es verhindert uebermaessiges clipping, gestattet jedoch dem getragenen Objekt ohne Kollision an Ecken vorbeibewegt zu werden.
-	 */
-	public class HoldInteraction : Interactable
-	{
-		private float distance = 2.5f;
-		private float smooth = 100;
 
-		private DummyColliderObject DummyObj;
-		private BoxCollider DummyCollider;
-		Rigidbody dummyRigid;
+    [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(Rigidbody))]
+    /**
+     * Ein Skript, dass es erlaubt, Objekte zu tragen, die einen Rigidbody und einen BoxCollider besitzen.
+     */
+    public class HoldInteraction : Interactable
+    {
+        [SerializeField]
+        float distance = 2.5f;
+        [SerializeField]
+        float minDistance = 0.5f;
+        [SerializeField]
+        float smoothnessOfInterpolation = 8;
 
-		private BoxCollider myCollider;
-		private Rigidbody myRigid;
+        Camera mainCamera;
+        GameObject Player;
+        private BoxCollider myCollider;
+        private Rigidbody myRigid;
+        Vector3 halfbox;
 
-		Vector3 Scale;
-		/**
-		 * Initialisiert die Variablen des Objekts und des DummyObjekts.
-		 */
-		void Awake()
-		{
-			myCollider = gameObject.GetComponent<BoxCollider>();
-			myRigid = gameObject.GetComponent<Rigidbody>();
-			GameObject Dummy = new GameObject();
-			DummyObj = Dummy.AddComponent<DummyColliderObject>();
-			DummyObj.distance = distance;
-			DummyObj.smooth = smooth;
-			Scale = gameObject.transform.lossyScale;
-			DummyObj.safe = gameObject.transform.position;
-			DummyObj.saferotate = gameObject.transform.rotation;
-			dummyRigid = Dummy.AddComponent<Rigidbody>();
-			dummyRigid.useGravity = false;
-			dummyRigid.collisionDetectionMode = CollisionDetectionMode.Continuous;
-			dummyRigid.isKinematic = true;
-			DummyCollider = Dummy.AddComponent<BoxCollider>();
-			DummyCollider.center = myCollider.center;
-			DummyCollider.size = LimitSize(Vector3.Scale(myCollider.size, Scale));
-		}
-		/**
-		 * Aktualisiert die Position des getragenen Objekts auf eine mit Lerp interpolierte Position zwischen der aktuellen Lage und einem sicheren Zielpunkt.
-		 */
-		void FixedUpdate()
-		{
-			if (DummyObj.carrying)
-			{
-				Vector3 NewLoc = Vector3.Lerp(gameObject.transform.position, DummyObj.safe, Time.deltaTime * 8);
-				gameObject.transform.position = NewLoc;
-				gameObject.transform.rotation = DummyObj.saferotate;//Quaternion.identity;
+        private bool carrying = false;
 
-			}
-		}
-		/**
-		 * Erlaubt es, Objekte fallen zu lassen.
-		 */ 
-		void Update()
-		{
-			if (DummyObj.carrying)
-			{
-				if (Input.GetButtonUp ("Interact")) drop();
-			}
-		}
-		public override string interactMessage()
-		{
-			return "carry";
-		}
-		/**
-		 * Hebt beim druecken des Knopfes das Objekt auf.
-		 */
-		public override void OnInteractionKeyPressed()
-		{
-			if (!DummyObj.carrying)
-			{
-				DummyObj.safe = gameObject.transform.position;
-				pickup();
-			}
-		}    
-		/**
-		 * Schaltet die Werte zurecht um das Tragen des Objekts zu ermoeglichen. Aktiviert die Interaktion mit dem Dummy-Objekt. Deaktiviert die Interaktionmit dem echten Objekt.
-		 */
-		private void pickup()
-		{
-			LogWriter.WriteLog("aufgehoben", gameObject);
-			myRigid.isKinematic = true;
-			myRigid.useGravity = false;
-			myCollider.enabled = false;
-			DummyCollider.enabled = true;
-			dummyRigid.isKinematic = false;
-			// DummyObj.safe = gameObject.transform.position;
-			DummyObj.carrying = true;
-		}
-		/**
-		 * Schaltet die Werte zurecht um das normale Verhalten des Objekts wiederherzustellen. Deaktiviert die Interaktionmit dem Dummy-Objekt. Aktiviert die Interaktion mit dem echten Objekt.
-		 */
-		private void drop()
-		{
-			LogWriter.WriteLog("fallen gelassen", gameObject);
-			dummyRigid.isKinematic = true;
-			myRigid.useGravity = true;
-			myRigid.isKinematic = false;
-			DummyCollider.enabled = false;
-			myCollider.enabled = true;
-			DummyObj.carrying = false;       
-		} 
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
 
-		/**
-		 * Limitiert Größe des Dummy-Colliders. Sorgt für Clipping bei zu großen Objekten. Ist der Boxcollider größer als 1 ist das erwartete Verhalten nicht mehr sichergestellt.
-		 */
-		private Vector3 LimitSize( Vector3 input)
-		{
-			Vector3 retVal;
-			if (input.x >= 1) retVal.x = 1;
-			else retVal.x = input.x;
-			if (input.y >= 1) retVal.y = 1;
-			else retVal.y = input.y;
-			if (input.z >= 1) retVal.z = 1; 
-			else retVal.z = input.z;
-			return retVal;
-		}
-	}
+        private Vector3 safe;
+        private Quaternion saferotate;
+        private Vector3 target;
+        private Quaternion tarRotation;
+        private int oldLayer;
+        private int newLayer = 2;
+
+        bool dropping = false;
+        /**
+         *Initialisieren von Vairablen. 
+         */
+        void Awake()
+        {
+            Player = GameObject.Find("FPSController");
+            mainCamera = Player.GetComponentInChildren<Camera>();
+            myCollider = gameObject.GetComponent<BoxCollider>();
+            myRigid = gameObject.GetComponent<Rigidbody>();
+            halfbox = myCollider.size * 0.5f;
+        }
+        /**
+         * Erneuert die Position des Objekts beim Tragen und erlaubt es das Tragen auch dann abzubrechen, wenn das Objekt eigentlich nicht in Reichweite ist. Bricht  Tragen ab, wenn unerlaubte Bewegungen versucht werden.
+         */
+        void Update()
+        {
+            if (carrying)
+            {
+                safe = mostDistantSafePosition();
+                if ((Input.GetButtonUp("Interact"))) drop();
+                Vector3 NewLoc = Vector3.Lerp(gameObject.transform.position, safe, Time.deltaTime * smoothnessOfInterpolation);
+                hasLineOfSight(NewLoc);
+                gameObject.transform.position = NewLoc;
+                gameObject.transform.rotation = saferotate;
+                float curDist = Vector3.Distance(mainCamera.transform.position, gameObject.transform.position);
+                if (curDist < minDistance*0.75 || curDist > distance*1.5) drop();
+
+            }
+        }
+        public override string interactMessage()
+        {
+            return "carry";
+        }
+        public override void OnInteractionKeyPressed()
+        {
+
+            if (carrying)
+            {
+                drop();
+            }
+        }
+
+        public override void OnInteractionKeyDown()
+        {
+            if (!carrying)
+            {
+                safe = gameObject.transform.position;
+                pickup();
+            }
+        }
+
+
+        /**
+        * Hebt das Objekt auf und setzt entsprechende Parameter.
+        */
+        private void pickup()
+        {
+            carrying = true;
+            LogWriter.WriteLog("aufgehoben", gameObject);
+            oldLayer = gameObject.layer;
+            gameObject.layer = newLayer;
+            myRigid.isKinematic = true;
+          myRigid.useGravity = false;
+
+        }
+        /**
+         * Laesst das Objekt fallen und setzt entsprechende Parameter.
+         */
+        private void drop()
+        {
+            carrying = false;
+            gameObject.layer = oldLayer;
+            LogWriter.WriteLog("fallen gelassen", gameObject);
+            myRigid.velocity = Vector3.zero;
+            myRigid.angularVelocity = Vector3.zero;
+
+            myRigid.isKinematic = false;
+            myRigid.useGravity = true;
+
+        }
+        /**
+         * Prueft ob an der Zielposition eine Kollision stattfinden wuerde.
+         */
+        private bool colliding()
+        {
+            return Physics.CheckBox(target, halfbox, tarRotation, Physics.DefaultRaycastLayers);
+        }
+
+        private bool inbetween(float Checkdistance)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Checkdistance + halfbox.z))
+            {
+                return true;
+            }
+            return false;
+        }
+        /**
+         * Sucht den am weitesten entfernten Punkt, auf dem das Objekt nicht kollidiert
+         */
+        private Vector3 mostDistantSafePosition()
+        {
+            float checkDistance = distance;
+
+            while (checkDistance > minDistance)
+            {
+                if (!inbetween(checkDistance))
+                {
+                    target = mainCamera.transform.position + mainCamera.transform.forward * checkDistance;
+                    tarRotation = Player.transform.rotation;
+                    if (!colliding())
+                    {
+                        saferotate = tarRotation;
+                        return target;
+                    }
+                }
+                checkDistance = checkDistance - 0.1f;
+
+            }
+            return safe;
+        }
+        /**
+         * Prueft, ob es eine direkte Linie zwischen der aktuellen und der geplanten Position gibt. 
+         */
+        private bool hasLineOfSight(Vector3 PositiontoTest)
+        {
+            Vector3 position = gameObject.transform.position;
+            Ray ray = new Ray(position, PositiontoTest - position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Vector3.Distance(PositiontoTest, position)))
+            {
+                if (hit.collider.gameObject.GetHashCode() == gameObject.GetHashCode())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
