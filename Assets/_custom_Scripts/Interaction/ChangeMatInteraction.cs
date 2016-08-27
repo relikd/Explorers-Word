58c536c788d21667402962ee55c55752b68dd2a0
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-27 17:22:54 +0200

    Raum 2 Fehler behoben und Skripte refactored Collect gibt es nicht mehr, da toggleInteraction das einsammeln bereits umfasst.  Cutinteraktion ist jetzt changeMatInteraction ReadInteraction ist jetzt sehr viel kürzer dank den public variablen des Players.

---
+++ Assets/_custom_Scripts/Interaction/ChangeMatInteraction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Interaction
+{
+
+    [RequireComponent (typeof(BoxCollider))]
+	/**
+	 * Erlaubt es das Material eines Objekts beim interagieren zu aendern. Die Interaktion kann nur einmal erfolgen.
+	 */
+	public class ChangeMatInteraction: Interactable
+	{
+		[SerializeField] 
+        string actionMessage; 
+        [SerializeField]
+        Material afterInteract;
+        [SerializeField]
+        BoxCollider BoxcolliderObj;
+        
+		/**
+		 * Gibt den Text zurueck, der dem Spieler angezeigt wird, wenn er mit der Maus über das interagierbare Objekt faehrt.
+		 */ 
+		override public string interactMessage() {
+			return actionMessage;
+		}
+
+		/**
+		 * Ersetzt das Material des Bilds und zerstört den BoxCollider des Gameobjects um eine erneute Interaktion zu verhinden.
+		 */
+		override public void OnInteractionKeyPressed()
+		{
+			LogWriter.WriteLog("Material gewechselt", gameObject);
+			gameObject.GetComponent<MeshRenderer>().material = afterInteract;
+            BoxcolliderObj.enabled = false;
+            this.enabled = false;
+		}
+	}
+}
