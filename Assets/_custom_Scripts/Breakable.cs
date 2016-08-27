e906a38c90099a2221a91c09a75a3a786294816b
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-27 23:18:19 +0200

    kleiner Fix an HoldInteraction, Prefab für zerbrechliche Truhe angepasst, altes prefab gelöscht, Anfang des Breakable Skripts (Zerbechen ist implementiert, Bedinungen und Auslöser noch nicht), Prefab für Überreste erstellt.

---
+++ Assets/_custom_Scripts/Breakable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Breakable : MonoBehaviour {
+    [SerializeField]
+    GameObject remains;
+
+    void Start () {
+	
+	}
+	void Update () {
+	}
+
+    public void shatter() 
+    {
+            Instantiate(remains, transform.position, transform.rotation);
+            Destroy(gameObject);
+    }
+}
