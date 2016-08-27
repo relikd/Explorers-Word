a4b1354752cb8afac04e39726d6261b5ab7e792f
Daniel Glück <hello@danielglueck.de>
2016-08-27 18:50:24 +0200

    Raum der Wünsche update XD Scripts für den Lüftungsschacht.

---
+++ Assets/_custom_Scripts/Interaction/RemoveScrews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Interaction;
+
+public class RemoveScrews : Interactable
+{
+	void Awake() {
+
+	}
+
+	public override string interactMessage () {			
+		return "Remove Screws";
+	}
+		
+	public override void OnInteractionKeyPressed () {
+		this.interactionEnabled = false;
+		//LevelManager.LoadNextRoom ();
+	}
+}
+
+
