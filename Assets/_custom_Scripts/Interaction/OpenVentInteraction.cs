75001173e71b5eda0b2dce617d72dee8f6f73a59
Daniel Glück <hello@danielglueck.de>
2016-08-27 18:50:24 +0200

    Raum der Wünsche update XD Scripts für den Lüftungsschacht.

---
+++ Assets/_custom_Scripts/Interaction/OpenVentInteraction.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Interaction;
+
+public class OpenVentInteraction : PlainInteraction {
+	
+	[SerializeField]private Interactable[] Screws;
+	bool removedAllScrews;
+	public string TriggerMessage;
+	public string CenteredMessage;
+
+	/**
+	 * Removes the assigned {@link PlainInteraction} on initialize		*/
+	void Awake() {
+
+	}
+
+	public override string interactMessage () {			
+		return responseMessage;
+	}
+
+	void LateUpdate() {
+		checkIfAllScrewsAreRemoved ();
+	}
+		
+	private void checkIfAllScrewsAreRemoved() {
+		int count = 0;
+		foreach (Interactable interact in Screws) {
+			if (!interact.interactionEnabled) {
+				count++;
+			} 
+		}
+		if (count == Screws.Length) {
+			removedAllScrews = true;
+			responseMessage = TriggerMessage;
+			centeredMessage (CenteredMessage);
+		}
+	}
+
+	public override void OnInteractionKeyPressed () {
+		Debug.Log (Screws.Length);
+		if (removedAllScrews) {
+			LevelManager.LoadNextRoom ();
+		}
+	}
+}
