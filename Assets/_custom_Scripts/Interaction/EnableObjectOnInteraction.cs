3f28a8a037e36770f6bda7d1fc47df075c26ec80
Daniel Glück <hello@danielglueck.de>
2016-08-28 17:38:13 +0200

    part of Dokumentation files.... Raum 3 Rätsel 1.0 ...wir müssen unbedingt die pivot punkte  bzw. eher die Rotationen verändern  :D LevelManager nur Laden von Text in bestimmten Räumen.

---
+++ Assets/_custom_Scripts/Interaction/EnableObjectOnInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Interaction;
+
+public class EnableObjectOnInteraction : PlainInteraction
+{
+	public bool disableSelfAfterUsage;
+	public Interactable[] InteractablesToDisable;
+	public GameObject[] GameObjects;
+
+	void LateUpdate() {
+		if (InteractablesToDisable != null && interactionEnabled) {
+			foreach (Interactable inter in InteractablesToDisable) {
+				inter.interactionEnabled = false;
+			}
+		}
+	}
+
+	public override void OnInteractionKeyPressed () {
+		centeredMessage (responseMessage);
+		foreach (GameObject go in GameObjects) {
+			go.SetActive (true);
+		}
+		if (disableSelfAfterUsage) {
+			this.interactionEnabled = false;
+		}
+	}
+}
+
