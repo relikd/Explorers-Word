bbffe672ff251d45ef327a2ec701b185ff005531
Jonas Kaiser <hello@kaiserjonas.de>
2016-08-28 19:53:01 +0200

    Raum 2 ist jetzt lösbar! Neue Skripte, die das ermöglichen. Ein paar Fehler in Skripten entfernt.

--- Assets/_custom_Scripts/Breakable/Breakable.cs
+++ Assets/_custom_Scripts/Breakable/Breakable.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+/*
+ * Ein Skript, mit dem ein Objekt zerbrochen werden kann, sofern die Einzelteile als Prefab uebergeben wurden. 
+ */
 public class Breakable : MonoBehaviour {
     [SerializeField]
     GameObject remains;
-
-    void Start () {
-	
-	}
-	void Update () {
-	}
-
+    [SerializeField]
+    float disappearAfterInSeconds;
+    /*
+     * Entfernt das Objekt und laed das Prefab fuer die Einzelteile an seine Position.
+     */
     public void shatter() 
     {
             Instantiate(remains, transform.position, transform.rotation);
---
+++ Assets/_custom_Scripts/Interaction/DropAndPullChainInteraction.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Interaction
+{
+    /*
+     * Ein Interaktionsskript, mit dem der Kronleuchter im zweiten Raum zu Fall gebreacht bzw. nach oben gezogen werden kann.
+     */
+    public class DropAndPullChainInteraction : Interactable
+    {
+        [SerializeField]
+        DropAndMoveChains ChainControlScript;
+        /*
+         * Laesst den Kronleuchter Fallen bzw. zieht ihn nach oben. Schaltet die angezeigte Nachricht um.
+         */ 
+        public override void OnInteractionKeyPressed()
+        {
+            EnableGUI(false);
+            if (!ChainControlScript.isDropping()) {
+                LogWriter.WriteLog("Kronleuchter fallen gelassen",gameObject);
+                ChainControlScript.drop();
+            }
+            else
+            {
+                LogWriter.WriteLog("Kronleuchter nach oben gezogen", gameObject);
+                ChainControlScript.pull();
+            }
+            EnableGUI(true);
+
+        }
+        /*
+         * Setzt die angezeigte Nachricht je nach aktuellem Zustand.
+         */
+        public override string interactMessage()
+        {
+            return !ChainControlScript.isDropping()? "Press to loosen chain": "Press to pull the chain";
+        }
+
+    }
+}
