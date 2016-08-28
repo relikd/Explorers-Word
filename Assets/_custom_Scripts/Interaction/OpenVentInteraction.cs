70b8a38342b05002c9ec340e5e401918a5da4a24
Daniel Glück <hello@danielglueck.de>
2016-08-28 17:50:27 +0200

    Documented the new Scripts

--- Assets/_custom_Scripts/Interaction/OpenVentInteraction.cs
+++ Assets/_custom_Scripts/Interaction/OpenVentInteraction.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using Interaction;
 
+/**
+* Functionality for the Vent Interaction in Room 3.
+*/
 public class OpenVentInteraction : PlainInteraction {
 	
 	[SerializeField]private Interactable[] Screws;
@@ -10,19 +13,22 @@ public class OpenVentInteraction : PlainInteraction {
 	public string CenteredMessage;
 
 	/**
-	 * Removes the assigned {@link PlainInteraction} on initialize		*/
-	void Awake() {
-
-	}
-
+	 * Gives the response Message.
+	 */
 	public override string interactMessage () {			
 		return responseMessage;
 	}
 
+	/**
+	 * Checks if all the Screws are removed.
+	 */
 	void LateUpdate() {
 		checkIfAllScrewsAreRemoved ();
 	}
 		
+	/**
+	 * Method for checking if the Screws are removed.
+	 */
 	private void checkIfAllScrewsAreRemoved() {
 		int count = 0;
 		foreach (Interactable interact in Screws) {
@@ -37,6 +43,9 @@ public class OpenVentInteraction : PlainInteraction {
 		}
 	}
 
+	/**
+	 * If all screws are removed and an interaction takes place, it loads the next Room .
+	 */
 	public override void OnInteractionKeyPressed () {
 		Debug.Log (Screws.Length);
 		if (removedAllScrews) {
