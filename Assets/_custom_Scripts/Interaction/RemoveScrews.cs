944b1184b043528d8800e49d8ff2cad8811e90b8
Daniel Glück <hello@danielglueck.de>
2016-08-28 17:50:27 +0200

    Documented the new Scripts

--- Assets/_custom_Scripts/Interaction/RemoveScrews.cs
+++ Assets/_custom_Scripts/Interaction/RemoveScrews.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using Interaction;
 
+/**
+* Remvoes Screws.
+*/
 public class RemoveScrews : Interactable
 {
-	void Awake() {
-
-	}
-
+	/**
+	 * shows an Interaction Message.
+	 */
 	public override string interactMessage () {			
 		return "Remove Screws";
 	}
 		
+	/**
+	 * Sets its own Interaction Possibility to false.
+	 */
 	public override void OnInteractionKeyPressed () {
 		this.interactionEnabled = false;
-		//LevelManager.LoadNextRoom ();
 	}
 }
 
