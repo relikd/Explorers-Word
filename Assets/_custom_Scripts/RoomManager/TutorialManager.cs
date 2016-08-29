e593bdf8bc3fb66b312e13c861c2fdda81b28efa
Daniel Glück <hello@danielglueck.de>
2016-08-29 20:20:25 +0200

    Tutorial Manager erneut angepasst

--- Assets/_custom_Scripts/RoomManager/TutorialManager.cs
+++ Assets/_custom_Scripts/RoomManager/TutorialManager.cs
@@ -6,14 +6,14 @@ using System.Collections.Generic;
 public class TutorialManager : MonoBehaviour
 {
 	public GUITutorialManager GUITutorialManager;
-	public string IntroText = "Aus zahlreichen Studien ist bekannt, dass sich ein Mensch maximal 3 Dinge gleichzeitig merken kann. Dies trift auch den Spieler dieser Welt zu, welcher nur 3 Dinge gleichzeitig visualisieren kann. Drücke die Rechte Pfeiltaste um fortzufahren.";
+	public string IntroTexte = "Aus zahlreichen Studien ist bekannt, dass sich ein Mensch maximal 3 Dinge gleichzeitig merken kann. Dies trift auch den Spieler dieser Welt zu, welcher nur 3 Dinge gleichzeitig visualisieren kann. \n Drücke die Rechte Pfeiltaste um fortzufahren.";
 	public string HowToDisplayTheBookText = "Um sich auch eine neue Sache konzentrieren und diese somit zu visualisieren muss der Spieler den Beschriebenen Gegenstand benennen. \n Drücke B um eine kurzbeschreibung deiner Umgebung zu bekommen und ein Eingabefeld anzuzeigen.";
-	public string OpenedTheBookText = "Sehr gut du hast erfolgreich das Buch geöffnet. Drücke die Rechte Pfeiltaste um fortzufahren.";
-	public string NameAnObjectText = "Nachdem du die Raumbeschreibung auf der rechten Seite gelesen hast kannst du ein Objekt nennen, welches sich wahrscheinlich im Raum befindet. \n Für den Anfang ist dort bereits ein richtiges Wort plaziert, alles was du noch tuhen musst ist, Enter zu drücken.";
+	public string OpenTheBookText = "Sehr gut du hast erfolgreich das Buch geöffnet. \n Drücke die Rechte Pfeiltaste um fortzufahren.";
+	public string NameAnObjectText = "Nachdem du die Raumbeschreibung auf der rechten Seite gelesen hast kannst du ein Objekt nennen, welches sich wahrscheinlich im Raum befindet. \n Für den Anfang ist dort bereits ein richtiges Wort plaziert, alles was du noch machen musst ist, Enter zu drücken.";
 	public string PressedEnterText = "Gut gemacht. Um wieder einen Blick auf den Raum zu werfen und sicher zu stellen, dass das genannte Objekt angezeigt wird, drücke Escape.";
-	public string VisibleText = "Wie du siehst ist das Objekt nun sichtbar." + " Um das Object erneut unsichtbar zu schalten musst du das Object erneut benennen. Öffne erneut das Buch und benenne das Object und schau dir an ob du das Objekt vergessen hast.";
-	public string ObjectInvisibleText = "Wie du siehst hast du das Objekt nun wieder vergessen. Drücke die Rechte Pfeiltaste um fortzufahren.";
-	public string ObjectOverflowText = "Nun weißt du wie man Objekte sichtbar visualisiert und vergisst, experimentiere wie man mehrere Objekt auf einmal zu visualisiert oder was passiert wenn man versucht sich zu viel auf einmal vorzustellen. Um das Tutorial zu verlassen laufe zur Tür und interagiere mit ihr.";
+	public string VisibleText = "Wie du siehst ist das Objekt nun sichtbar. \n Um das Object erneut unsichtbar zu schalten musst du das Object erneut benennen. Öffne erneut das Buch und benenne das Object und schau dir an ob du das Objekt vergessen hast.";
+	public string InvisibleText = "Wie du siehst hast du das Objekt nun wieder vergessen. \n Drücke die Rechte Pfeiltaste um fortzufahren.";
+	public string OverflowText = "Nun weißt du wie man Objekte sichtbar visualisiert und vergisst, experimentiere wie man mehrere Objekt auf einmal zu visualisiert oder was passiert wenn man versucht sich zu viel auf einmal vorzustellen. \n Um das Tutorial zu verlassen laufe zur Tür und interagiere mit ihr.";
 	bool tutorialInitilization = false;
 	bool shouldDisplayOpenBookText = false;
 	bool shouldOpenBook = false;
@@ -34,10 +34,10 @@ public class TutorialManager : MonoBehaviour
 			GameManager.getInstance ().disableRightBookPage ();
 			TutorialIntitialization ();
 			tutorialInitilization = true;
-			GUITutorialManager.register (IntroText, true);
+			GUITutorialManager.register (IntroTexte, true);
 		}
 		if (shouldDisplayOpenBookText && Input.GetKeyDown (KeyCode.RightArrow)) {
-			GUITutorialManager.register (IntroText, false);
+			GUITutorialManager.register (IntroTexte, false);
 			GUITutorialManager.register (HowToDisplayTheBookText, true);
 			lockUserControlls ();
 			GameManager.getInstance ().disableExplorersBook ();
@@ -48,12 +48,12 @@ public class TutorialManager : MonoBehaviour
 		} else if (shouldOpenBook && Input.GetKeyDown (KeyCode.B)) {
 			//lockUserControlls ();
 			GUITutorialManager.register (HowToDisplayTheBookText, false);
-			GUITutorialManager.register (OpenedTheBookText, true);
+			GUITutorialManager.register (OpenTheBookText, true);
 
 			shouldOpenBook = false;
 			shouldNameAnObject = true;
 		} else if (shouldNameAnObject && Input.GetKeyDown (KeyCode.RightArrow)) {
-			GUITutorialManager.register (OpenedTheBookText, false);
+			GUITutorialManager.register (OpenTheBookText, false);
 			GUITutorialManager.register (NameAnObjectText, true);
 			GUITutorialManager.WriteToInputField ("books");
 
@@ -74,13 +74,13 @@ public class TutorialManager : MonoBehaviour
 			shouldNameObjectAgain = true;
 		} else if (shouldNameObjectAgain && Input.GetKeyDown (KeyCode.Escape)) {
 			GUITutorialManager.register (VisibleText, false);
-			GUITutorialManager.register (ObjectInvisibleText, true);
+			GUITutorialManager.register (InvisibleText, true);
 
 			shouldNameObjectAgain = false;
 			shouldExperiment = true;
 		} else if (shouldExperiment && Input.GetKeyDown (KeyCode.RightArrow)) {
-			GUITutorialManager.register (ObjectInvisibleText, false);
-			GUITutorialManager.register (ObjectOverflowText, true);
+			GUITutorialManager.register (InvisibleText, false);
+			GUITutorialManager.register (OverflowText, true);
 			shouldExperiment = false;
 		}
 	}
