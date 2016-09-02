db2c8272e28ba9d0fc91bde6f8456a3df802b5a0
Daniel Mügge <hello@daniel-muegge.de>
2016-09-02 15:32:58 +0200

    Finished modeling Room 2 with kitchen (saved under room2_new) . Look for yourself! Added new Script for Watermovement in kitchensink. "Water Movement Script" New Asset KITCHEN!!! a bit modern but i don't care because this game is inside a GOD DAMN DREAM! :D

---
+++ Assets/_custom_Scripts/WaterMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterMovementScript : MonoBehaviour {
+
+	public float movementSize;
+	public Vector3 movementAxis;
+	private int moveDir = -1;
+	public int steps = 20;
+
+	private int counter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (moveDir == 1) {
+			this.transform.position += new Vector3 (
+				movementAxis.x * movementSize,
+				movementAxis.y * movementSize,
+				movementAxis.z *  movementSize
+			);
+			counter++;
+		}
+		if (moveDir == -1) {
+			this.transform.position -= new Vector3 (
+				movementAxis.x * movementSize,
+				movementAxis.y * movementSize,
+				movementAxis.z *  movementSize
+			);
+			counter++;
+		}
+
+		if (counter >= steps) {
+			counter = 0;
+			moveDir = moveDir * (-1);
+		}
+
+	}
+}
