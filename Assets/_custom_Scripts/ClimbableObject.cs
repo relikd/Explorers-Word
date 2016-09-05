58a9fdfb5e5d1c93b2721e7c947fbe0c766c46ea
Daniel Mügge <hello@daniel-muegge.de>
2016-09-05 13:12:26 +0200

    Room 4 nearly finished in means of modeling // ClimbableObject Script for Room 4 // New Assets Pack: Medival House (used for GardenHut in Room 4 and Coach )// WaterBasic/ProDaytime/NightTime shader fix. Working again

---
+++ Assets/_custom_Scripts/ClimbableObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbableObject : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject ground;
+	public XplrCharacter.FPSController fps_controller;
+
+	private BoxCollider climbCollider;
+
+	private bool onWall = false;
+
+	private float distanceToGround;
+
+	// Use this for initialization
+	void Start () {
+		climbCollider = this.gameObject.GetComponent<BoxCollider> ();
+		climbCollider.isTrigger = true;
+	}
+
+	void Update(){
+		distanceToGround = Vector3.Distance (player.transform.position, ground.transform.position);
+		if (distanceToGround < 0.1) {
+			onWall = false;
+		}
+	}
+
+	void FixedUpdate(){
+
+		if (onWall && Input.GetKey (KeyCode.W)) {
+			player.GetComponent<Transform> ().position += new Vector3 (0, 0.05f, 0);
+		}
+		if (onWall && Input.GetKeyUp (KeyCode.Space)) {
+			player.GetComponent<Transform> ().position += new Vector3 (0, 0, 0.5f);
+			onWall = false;
+		}
+		if (onWall && distanceToGround > 0.2 && Input.GetKey(KeyCode.S)) {
+			player.GetComponent<Transform> ().position -= new Vector3 (0, 0.05f, 0);
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		player.GetComponent<Rigidbody> ().mass = 0.0f;
+		onWall = true;
+		fps_controller.shouldJump = false;
+		fps_controller.shouldWalk = false;
+		fps_controller.shouldPlayAudioSounds = false;
+		fps_controller.m_UseHeadBob = false;
+	}
+
+	void OnTriggerExit(Collider col){
+		player.GetComponent<Rigidbody> ().mass = 1.0f;
+		onWall = true;
+		fps_controller.shouldJump = true;
+		fps_controller.shouldWalk = true;	
+		fps_controller.shouldPlayAudioSounds = true;
+		fps_controller.m_UseHeadBob = true;
+	}
+
+}
--- Assets/_custom_Scripts/WaterMovementScript.cs
+++ Assets/_custom_Scripts/WaterMovementScript.cs
@@ -15,7 +15,7 @@ public class WaterMovementScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 		if (moveDir == 1) {
 			this.transform.position += new Vector3 (
