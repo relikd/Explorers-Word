ce8de4d2e1e877ec68f8a07d52105f421cc958f3
Daniel Mügge <hello@daniel-muegge.de>
2016-09-05 19:54:40 +0200

    Models for ToyKnights. Now inside the Chest beneath the long painting. Changed Puzzle Room 2. Room2 finished. the words need to be updated Room 2. Sounds is missing for new Interactions.

---
+++ Assets/_custom_Scripts/Interaction/AnimationInteraction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Interaction
+{
+	/**
+	 * Handle an interaction which is evaluated by a second script
+	 */
+	public class AnimationInteraction : PlainInteraction
+	{
+		public Animation animationToPlay;
+		public bool animateOnce;
+		public bool deactCollider;
+		public Collider collider;
+
+		/**
+		 * Will run the attached animation and display the message
+		 */
+		override public void OnInteractionKeyPressed() {
+
+			animationToPlay.Play ();
+
+			if (animateOnce)
+				this.interactionEnabled = false;
+			if (deactCollider) {
+				collider.enabled = false;
+			}
+			centeredMessage (responseMessage);
+		}
+	}
+}
