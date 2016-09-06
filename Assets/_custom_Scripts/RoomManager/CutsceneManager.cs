ed9687e8bb4b38b449402010bf552cf8414fc974
Daniel Mügge <hello@daniel-muegge.de>
2016-09-06 19:52:39 +0200

    Updated Cutscene script for Oleg

--- Assets/_custom_Scripts/RoomManager/CutsceneManager.cs
+++ Assets/_custom_Scripts/RoomManager/CutsceneManager.cs
@@ -6,21 +6,37 @@ public class CutsceneManager : MonoBehaviour {
 	public AudioClip cutsceneAudioClip;
 	private AudioSource audioSource;
 	private bool audioHasStarted = false;
-	// Use this for initialization
-	void Start () {
-		audioSource = gameObject.AddComponent<AudioSource> ();
-		audioSource.clip = cutsceneAudioClip;
-		audioSource.loop = false;
-		audioSource.volume = 1.0f;
-		audioSource.Play ();
 
-		audioHasStarted = true;
+//	public static int CutsceneIndex 
+//	{
+//		get;
+//		set;
+//	}
+
+	void Start () 
+	{
+		StartCutsceneAudio ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (audioHasStarted && !audioSource.isPlaying) {
+		if (audioHasStarted 
+			&& !audioSource.isPlaying
+			&& cutsceneAudioClip != null) {
 			LevelManager.LoadRoom ("room_1");
+		}
+	}
+
+	void StartCutsceneAudio()
+	{
+		if (cutsceneAudioClip != null) {
+			audioSource = gameObject.AddComponent<AudioSource> ();
+			audioSource.clip = cutsceneAudioClip;
+			audioSource.loop = false;
+			audioSource.volume = 1.0f;
+			audioSource.Play ();
+
+			audioHasStarted = true;
 		}
 	}
 }
