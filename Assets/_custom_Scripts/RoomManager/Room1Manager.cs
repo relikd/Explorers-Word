ed9c265c068f868802c6d00f4de3093217eea68f
Oleg Geier <oleg@relikd.de>
2016-09-07 16:03:44 +0200

    namespace RoomManager

--- Assets/_custom_Scripts/RoomManager/Room1Manager.cs
+++ Assets/_custom_Scripts/RoomManager/Room1Manager.cs
@@ -1,138 +1,139 @@
 ﻿using UnityEngine;
 
-/**
- * Evalutate level state for Room 1.
- * All lightbeams have to be rotated to the correct position
- */
-public class Room1Manager : MonoBehaviour {
-
-	[SerializeField] private GameObject startingLightbeam;
-	[SerializeField] private GameObject finishCrystal;
-	[SerializeField] private int lightsRequiredToFinish = 3;
-	[SerializeField] private GameObject[] rotatableObjects;
-
-	/** Puzzle is solved if the right amount of light hits the final crystal */
-	private int currentLightsOnFinish = 0;
-	/** Flag used to recast lightbeams (saves calculation time) */
-	private bool shouldRecalculate = false;
-	[SerializeField] private GameObject mayaBook;
-	[SerializeField] private GameObject globe;
-	[SerializeField] private GameObject raumDesign;
-	private int beenThereDoneThat;
-
-	// only used to get an redraw message from after a text input
-	void OnEnable () { Events.instance.AddListener<XplrEvents.WordEntered>(OnNewWordEntered); }
-	void OnDisable () { Events.instance.RemoveListener<XplrEvents.WordEntered>(OnNewWordEntered); }
-	/** Called every time user enters something in the Explorer's Book */
-	void OnNewWordEntered (XplrEvents.WordEntered e) {
-		if (e.exists)
-			shouldRecalculate = true;
-	}
-
-	void Start () {
-		initialPuzzleTwist ();
-	}
+namespace RoomManager
+{
 	/**
-	 * Will rotate the initial correct state to a wrong one. So the puzzle isn't solved immediatelly
+	 * Evalutate puzzle for Room 1. All lightbeams have to be rotated to the correct position
 	 */
-	void initialPuzzleTwist () {
-		foreach (GameObject item in rotatableObjects) {
-			Interaction.RotateInteraction ri_script = item.GetComponent<Interaction.RotateInteraction> ();
-			if (ri_script)
-				item.transform.Rotate (ri_script.rotateBy*2);
+	public class Room1Manager : MonoBehaviour {
+
+		[SerializeField] private GameObject startingLightbeam;
+		[SerializeField] private GameObject finishCrystal;
+		[SerializeField] private int lightsRequiredToFinish = 3;
+		[SerializeField] private GameObject[] rotatableObjects;
+
+		/** Puzzle is solved if the right amount of light hits the final crystal */
+		private int currentLightsOnFinish = 0;
+		/** Flag used to recast lightbeams (saves calculation time) */
+		private bool shouldRecalculate = false;
+		[SerializeField] private GameObject mayaBook;
+		[SerializeField] private GameObject globe;
+		[SerializeField] private GameObject raumDesign;
+		private int beenThereDoneThat;
+
+		// only used to get an redraw message from after a text input
+		void OnEnable () { Events.instance.AddListener<XplrEvents.WordEntered>(OnNewWordEntered); }
+		void OnDisable () { Events.instance.RemoveListener<XplrEvents.WordEntered>(OnNewWordEntered); }
+		/** Called every time user enters something in the Explorer's Book */
+		void OnNewWordEntered (XplrEvents.WordEntered e) {
+			if (e.exists)
+				shouldRecalculate = true;
 		}
-	}
-	/**
-	 * If game state changes it will recast all lightbeams, otherwise only maya book animation
-	 */
-	void Update () {
-		activateRoomDesign ();
-		if (currentLightsOnFinish >= lightsRequiredToFinish)
-			activateMayaBook (true); // will be called continuously for glow effect
 
-		foreach (GameObject item in rotatableObjects)
-			shouldRecalculate |= item.transform.hasChanged;
-
-		if (shouldRecalculate) {
-			shouldRecalculate = false;
+		void Start () {
+			initialPuzzleTwist ();
+		}
+		/**
+		 * Will rotate the initial correct state to a wrong one. So the puzzle isn't solved immediatelly
+		 */
+		void initialPuzzleTwist () {
+			foreach (GameObject item in rotatableObjects) {
+				Interaction.RotateInteraction ri_script = item.GetComponent<Interaction.RotateInteraction> ();
+				if (ri_script)
+					item.transform.Rotate (ri_script.rotateBy*2);
+			}
+		}
+		/**
+		 * If game state changes it will recast all lightbeams, otherwise only maya book animation
+		 */
+		void Update () {
+			activateRoomDesign ();
+			if (currentLightsOnFinish >= lightsRequiredToFinish)
+				activateMayaBook (true); // will be called continuously for glow effect
+	
 			foreach (GameObject item in rotatableObjects)
-				item.transform.hasChanged = false;
+				shouldRecalculate |= item.transform.hasChanged;
+	
+			if (shouldRecalculate) {
+				shouldRecalculate = false;
+				foreach (GameObject item in rotatableObjects)
+					item.transform.hasChanged = false;
+				
+				clearAllLights ();
+				lightHitObject (startingLightbeam.transform);
+			}
+		}
+		/**
+		 * Deactivates all lightbeams and set solved state to false
+		 */
+		void clearAllLights() {
+			// initially disable all lightbeams
+			GameObject[] lights = GameObject.FindGameObjectsWithTag("lightbeam");
+			foreach(GameObject light in lights)
+				light.SetActive (false);
+			currentLightsOnFinish = 0;
+			activateGlobe (false);
+			activateMayaBook (false);
+		}
+		/**
+		 * Will recursively cast lightbeams for all children of target object
+		 */
+		void lightHitObject(Transform t) {
+			// check for special objects
+			if (t == null)
+				return;
+			else if (t == finishCrystal.transform)
+				currentLightsOnFinish++;
+			else if (t == globe.transform)
+				activateGlobe (true);
 			
-			clearAllLights ();
-			lightHitObject (startingLightbeam.transform);
+			// go recursively through all children
+			for (int i = 0; i < t.childCount; i++) {
+				Transform child = t.GetChild (i);
+				lightHitObject (child);
+			}
+			// if light beam then recast
+			LightbeamExpansion lbe_script = t.GetComponent<LightbeamExpansion> ();
+			if (lbe_script && !t.gameObject.activeSelf) {
+				t.gameObject.SetActive (true);
+				lightHitObject (lbe_script.Expand ());
+			}
 		}
-	}
-	/**
-	 * Deactivates all lightbeams and set solved state to false
-	 */
-	void clearAllLights() {
-		// initially disable all lightbeams
-		GameObject[] lights = GameObject.FindGameObjectsWithTag("lightbeam");
-		foreach(GameObject light in lights)
-			light.SetActive (false);
-		currentLightsOnFinish = 0;
-		activateGlobe (false);
-		activateMayaBook (false);
-	}
-	/**
-	 * Will recursively cast lightbeams for all children of target object
-	 */
-	void lightHitObject(Transform t) {
-		// check for special objects
-		if (t == null)
-			return;
-		else if (t == finishCrystal.transform)
-			currentLightsOnFinish++;
-		else if (t == globe.transform)
-			activateGlobe (true);
-		
-		// go recursively through all children
-		for (int i = 0; i < t.childCount; i++) {
-			Transform child = t.GetChild (i);
-			lightHitObject (child);
+		/**
+		 * Sets the design texture for the room
+		 */
+		void activateRoomDesign() {
+			if (beenThereDoneThat == 0 && Camera.main.transform.position.x < -9)
+				beenThereDoneThat = 1;
+			if (beenThereDoneThat == 1 && Camera.main.transform.position.x > -5)
+				beenThereDoneThat = 2;
+			raumDesign.SetActive (beenThereDoneThat == 2);
 		}
-		// if light beam then recast
-		LightbeamExpansion lbe_script = t.GetComponent<LightbeamExpansion> ();
-		if (lbe_script && !t.gameObject.activeSelf) {
-			t.gameObject.SetActive (true);
-			lightHitObject (lbe_script.Expand ());
+		/**
+		 * Activates the easter egg
+		 */
+		void activateGlobe(bool flag) {
+			Renderer renderer = globe.GetComponent <Renderer>();
+			Color emissisonColor = ( flag ? Color.white : Color.black );
+			if (flag) emissisonColor.a = 0.5f;
+			renderer.material.SetColor ("_EmissionColor", emissisonColor);
 		}
-	}
-	/**
-	 * Sets the design texture for the room
-	 */
-	void activateRoomDesign() {
-		if (beenThereDoneThat == 0 && Camera.main.transform.position.x < -9)
-			beenThereDoneThat = 1;
-		if (beenThereDoneThat == 1 && Camera.main.transform.position.x > -5)
-			beenThereDoneThat = 2;
-		raumDesign.SetActive (beenThereDoneThat == 2);
-	}
-	/**
-	 * Activates the easter egg
-	 */
-	void activateGlobe(bool flag) {
-		Renderer renderer = globe.GetComponent <Renderer>();
-		Color emissisonColor = ( flag ? Color.white : Color.black );
-		if (flag) emissisonColor.a = 0.5f;
-		renderer.material.SetColor ("_EmissionColor", emissisonColor);
-	}
-	/**
-	 * Will activate the book glow and trigger interaction
-	 */
-	void activateMayaBook (bool flag) {
-		Color finalColor = Color.black;
-		if (flag) {
-			float emission = 0.01f + Mathf.PingPong (Time.time, 0.7f) / 2.0f;
-			Color baseColor = Color.yellow;
-			finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
-		}
+		/**
+		 * Will activate the book glow and trigger interaction
+		 */
+		void activateMayaBook (bool flag) {
+			Color finalColor = Color.black;
+			if (flag) {
+				float emission = 0.01f + Mathf.PingPong (Time.time, 0.7f) / 2.0f;
+				Color baseColor = Color.yellow;
+				finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+			}
+			Renderer renderer =	mayaBook.GetComponent<Renderer> ();
+			Material mat = renderer.material;
+			mat.SetColor ("_EmissionColor", finalColor);
 
-		Renderer renderer =	mayaBook.GetComponent<Renderer> ();
-		Material mat = renderer.material;
-		mat.SetColor ("_EmissionColor", finalColor);
-
-		Interaction.TriggerInteraction script = mayaBook.GetComponent<Interaction.TriggerInteraction> ();
-		script.triggerActive = flag;
+			Interaction.TriggerInteraction script = mayaBook.GetComponent<Interaction.TriggerInteraction> ();
+			script.triggerActive = flag;
+		}
 	}
 }
--- Assets/_custom_Scripts/RoomManager/RoomEndOfGameManager.cs
+++ Assets/_custom_Scripts/RoomManager/RoomEndOfGameManager.cs
@@ -1,69 +1,71 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class RoomEndOfGameManager : MonoBehaviour {
+namespace RoomManager
+{
+	public class RoomEndOfGameManager : MonoBehaviour {
 
-	public Canvas creditCanvas;
-	public Camera mainCamera;
-	public Transform whitePortalBorder;
-	public Transform blackPortalBorder;
-	private CameraShakeScript cameraShaker;
-	private Transform textObject;
-	private Text creditText;
+		public Canvas creditCanvas;
+		public Camera mainCamera;
+		public Transform whitePortalBorder;
+		public Transform blackPortalBorder;
+		private CameraShakeScript cameraShaker;
+		private Transform textObject;
+		private Text creditText;
 
-
-	// Use this for initialization
-	void Start () {
-		mainCamera = mainCamera.GetComponent<Camera> ();
-		cameraShaker = mainCamera.GetComponent<CameraShakeScript> ();
-		whitePortalBorder = whitePortalBorder.GetComponent<Transform> ();
-		blackPortalBorder = blackPortalBorder.GetComponent<Transform> ();
-		creditCanvas.enabled = false;
-	}
-
-	void Update(){
-		float distanceWhite = Vector3.Distance (mainCamera.transform.position, whitePortalBorder.transform.position);
-		float distanceBlack = Vector3.Distance (mainCamera.transform.position, blackPortalBorder.transform.position);
-
-		Debug.Log ("Distance white: " + distanceWhite.ToString () + " / Distance black: " + distanceBlack.ToString ());
-		if (distanceBlack < 80 || distanceWhite < 80) {
-			cameraShaker.shakeAmount = 0.4f;
-		}
-		else if (distanceBlack < 70 || distanceWhite < 70) {
-			cameraShaker.shakeAmount = 0.6f;
-		} else if (distanceBlack < 50 || distanceWhite < 50) {
-			cameraShaker.shakeAmount = 1.0f;
-		} else if (distanceBlack < 30 || distanceWhite < 30) {
-			cameraShaker.shakeAmount = 2.0f;
-		} else {
-			cameraShaker.shakeAmount = 0.0f;
+		// Use this for initialization
+		void Start () {
+			mainCamera = mainCamera.GetComponent<Camera> ();
+			cameraShaker = mainCamera.GetComponent<CameraShakeScript> ();
+			whitePortalBorder = whitePortalBorder.GetComponent<Transform> ();
+			blackPortalBorder = blackPortalBorder.GetComponent<Transform> ();
+			creditCanvas.enabled = false;
 		}
 
-		if (textObject != null) {
-			if (textObject.GetComponent<RectTransform>().position.y >= 1400) {
-				LevelManager.LoadStartScreen ();
+		void Update(){
+			float distanceWhite = Vector3.Distance (mainCamera.transform.position, whitePortalBorder.transform.position);
+			float distanceBlack = Vector3.Distance (mainCamera.transform.position, blackPortalBorder.transform.position);
+
+			Debug.Log ("Distance white: " + distanceWhite.ToString () + " / Distance black: " + distanceBlack.ToString ());
+			if (distanceBlack < 80 || distanceWhite < 80) {
+				cameraShaker.shakeAmount = 0.4f;
+			}
+			else if (distanceBlack < 70 || distanceWhite < 70) {
+				cameraShaker.shakeAmount = 0.6f;
+			} else if (distanceBlack < 50 || distanceWhite < 50) {
+				cameraShaker.shakeAmount = 1.0f;
+			} else if (distanceBlack < 30 || distanceWhite < 30) {
+				cameraShaker.shakeAmount = 2.0f;
+			} else {
+				cameraShaker.shakeAmount = 0.0f;
+			}
+
+			if (textObject != null) {
+				if (textObject.GetComponent<RectTransform>().position.y >= 1400) {
+					LevelManager.LoadStartScreen ();
+				}
 			}
 		}
-	}
 
-	void OnTriggerEnter(Collider col){
+		void OnTriggerEnter(Collider col){
 
-		XplrCharacter.FPSController fps_script = GetComponent<XplrCharacter.FPSController> ();
-		fps_script.shouldWalk = false;
-		fps_script.shouldJump = false;
-		fps_script.shouldLookAround = false;
+			XplrCharacter.FPSController fps_script = GetComponent<XplrCharacter.FPSController> ();
+			fps_script.shouldWalk = false;
+			fps_script.shouldJump = false;
+			fps_script.shouldLookAround = false;
 
-		float lookDirection = mainCamera.transform.rotation.eulerAngles.y;
-		bool blackEnd = (lookDirection >= 0 && lookDirection < 180);
-		XplrDebug.LogWriter.Write ("Game End: "+ (blackEnd ? "BLACK" : "WHITE"));
+			float lookDirection = mainCamera.transform.rotation.eulerAngles.y;
+			bool blackEnd = (lookDirection >= 0 && lookDirection < 180);
+			XplrDebug.LogWriter.Write ("Game End: "+ (blackEnd ? "BLACK" : "WHITE"));
 
-		// background color
-		creditCanvas.GetComponent<Image> ().color = (blackEnd ? Color.black : Color.white);
-		creditCanvas.enabled = true;
-		textObject = creditCanvas.transform.FindChild("Text");
-		creditText = textObject.GetComponent<Text> ();
-		// text color
-		creditText.color = (blackEnd ? Color.white : Color.black);
-		creditText.GetComponent<CreditsTranslation> ().StartCredits ();
+			// background color
+			creditCanvas.GetComponent<Image> ().color = (blackEnd ? Color.black : Color.white);
+			creditCanvas.enabled = true;
+			textObject = creditCanvas.transform.FindChild("Text");
+			creditText = textObject.GetComponent<Text> ();
+			// text color
+			creditText.color = (blackEnd ? Color.white : Color.black);
+			creditText.GetComponent<CreditsTranslation> ().StartCredits ();
+		}
 	}
 }
