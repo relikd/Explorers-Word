be95ee2468af3f14330afcfe2fc08b2e82edd95f
Oleg Geier <oleg@relikd.de>
2016-09-16 23:30:25 +0200

    1.3 nicht 1.3.0

--- Assets/_custom_Scripts/RoomManager/MainMenuScript.cs
+++ Assets/_custom_Scripts/RoomManager/MainMenuScript.cs
@@ -9,7 +9,7 @@ namespace RoomManager{
 	/// </summary>
 	public class MainMenuScript : MonoBehaviour {
 
-		private string version = "1.3.0";
+		private string version = "1.3";
 		public Text versionString;
 
 		public Button startGameButton;
