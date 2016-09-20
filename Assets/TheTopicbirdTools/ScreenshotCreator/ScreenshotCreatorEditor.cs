6c3b5887298834d6646be929fd996e06026154c8
Jonas Kaiser <hello@kaiserjonas.de>
2016-09-20 19:26:25 +0200

    Screenshot tools Das Custom script ist für Screenshots beim Spielen (Nicht im echten Build drinnen, exsisitert nur für Doku Zwecke) Das andere ist aus dem Assets store und ermöglicht Screenshots im Editor Sceneview

---
+++ Assets/TheTopicbirdTools/ScreenshotCreator/ScreenshotCreatorEditor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(ScreenshotCreator))]
+public class ScreenshotCreatorEditor : Editor {
+	[SerializeField] ScreenshotCreator script;
+
+	void OnEnable(){
+		script = (ScreenshotCreator)target;
+	}
+
+	// reset all X questions to standard
+	void OnDisable(){
+		refreshRequests ();
+	}
+	
+	void refreshRequests(){
+		for (int i = 0; i < script.list.Count; i++) {
+			script.list[i].deleteQuestion = false;
+		}
+	}
+
+	public override void OnInspectorGUI() {
+		EditorUtility.SetDirty (target);
+
+		script.superSize = EditorGUILayout.IntSlider("size factor", script.superSize, 1, 16);
+		EditorGUILayout.LabelField(Screen.width * script.superSize + "x" + Screen.height * script.superSize);
+
+		EditorGUILayout.Space ();
+
+		script.screenshotName = EditorGUILayout.TextField("name", script.screenshotName);
+		EditorGUILayout.SelectableLabel("save path = " + System.IO.Directory.GetCurrentDirectory() + "/Screenshots/");
+
+		for (int i = 0; i < script.list.Count; i++) {
+			ScreenshotCreator.CameraObject c = script.list[i];
+
+			EditorGUILayout.BeginHorizontal ();
+
+			GUI.color = Color.white;
+			script.list[i].cam = (GameObject) EditorGUILayout.ObjectField(script.list[i].cam, typeof(GameObject), true);
+
+			EditorGUI.BeginDisabledGroup (!EditorApplication.isPlaying);
+			if (script.list [i].cam != null) {
+				if (GUILayout.Button ("USE " + script.list [i].cam.name, new GUIStyle(EditorStyles.miniButtonLeft))) {
+					refreshRequests();
+					script.CaptureScreenshots (i);
+				}
+			}
+			EditorGUI.EndDisabledGroup();
+
+			// the delete button
+			if (c.deleteQuestion){
+				GUI.color = Color.red;
+				if (GUILayout.Button ("YES?", new GUIStyle(EditorStyles.miniButtonRight), GUILayout.MaxWidth(45), GUILayout.MaxHeight(14))) {
+					refreshRequests();
+					script.Delete (i);
+				}
+			} else {
+				GUI.color = (Color.red + Color.white) / 2f;
+				if (GUILayout.Button ("X", new GUIStyle(EditorStyles.miniButtonRight), GUILayout.MaxWidth(45), GUILayout.MaxHeight(14))) {
+					refreshRequests();
+					script.RequestDelete (i);
+				}
+			}
+
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.Space ();
+		}
+
+		GUI.color = new Color (0.54f, 0.68f, 0.95f);
+		if(GUILayout.Button("ADD CAMERA", GUILayout.MaxWidth (100), GUILayout.MinWidth (100), GUILayout.MaxHeight (25), GUILayout.MinHeight (25))) {
+			refreshRequests();
+			script.Create ();
+		}
+	}
+}
+#endif
---
+++ Assets/_custom_Scripts/Generic/ScreenshotTool.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+/**
+ * A simple script to take screenshots whitin the game. The Screenshot contains only the actual game screen without any parts of the OS Gui or the Unity Editor. It is not part of the game but a tool for the developers that allows easier documentation.
+ */ 
+public class ScreenshotTool : MonoBehaviour {
+
+    private int count = 0;
+    private string dir = "Screenshots";
+
+    /**
+    * Creates the Folder if it doesn't exist and  get the number for the next screenshot.
+    */ 
+    void Start () {
+        if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+        DirectoryInfo dirinf = new DirectoryInfo(dir);
+        FileInfo[] info = dirinf.GetFiles("Screenshot*");
+        foreach (FileInfo f in info)
+        {
+            string[] substrings = f.Name.Split('.');
+            int filenumber = int.Parse(substrings[0].Substring(10));
+            if (filenumber >= count) count = filenumber + 1;
+        }
+    }
+
+    /**
+    * Takes a Screenshot if 'X' is released and increases the count by 1.
+    */
+    void Update () {
+        if (Input.GetKeyUp(KeyCode.X))       
+            Application.CaptureScreenshot("Screenshots/Screenshot" + count++ + ".png");
+        }
+}
