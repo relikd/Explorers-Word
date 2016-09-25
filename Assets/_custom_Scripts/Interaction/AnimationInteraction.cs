d0f34c761ee01d68d85cb649b008f1346378e396
relikd <info@relikd.de>
2016-09-25 22:23:16 +0200

    Reset the worst DESTRUCTIVE commit ever. And that on the very last day! Whoever did that should pay a round of beer to all other project members.
    thanks Lucky for our beverages

--- Assets/_custom_Scripts/Interaction/AnimationInteraction.cs
+++ Assets/_custom_Scripts/Interaction/AnimationInteraction.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using XplrDebug;
-using System.Collections;
 
 /** Contains all classes with which the user can interact */
 namespace Interaction
@@ -10,42 +9,25 @@ namespace Interaction
 	 */
 	public class AnimationInteraction : PlainInteraction
 	{
-		public Animator animationToPlay;
+		public Animation animationToPlay;
+		public bool animateOnce;
+		public bool deactColliderAfterwards;
+		public Collider colliderToDeactivate;
 
 		/**
-		 * Will run the attached animator, wich hopefully contains an Animation that is handle over a State machine, and display the message
+		 * Will run the attached animation and display the message
 		 */
 		override public void OnInteractionKeyPressed() {
 
 			LogWriter.Write ("Animation abgespielt: " + animationToPlay.name, this.gameObject);
-			StartCoroutine (playAnimation());
+			animationToPlay.Play ();
+
+			if (animateOnce)
+				this.interactionEnabled = false;
+			if (deactColliderAfterwards) {
+				colliderToDeactivate.enabled = false;
+			}
 			centeredMessage (responseMessage);
-			interactionEnabled = false;
-
 		}
-	
-	/**
-	 * Plays the Animation of the attached Animator and waits while its playing
-	 * @return duration in seconds
-	 */
-	IEnumerator playAnimation() {
-		Animator anim = this.animationToPlay;
-		if (anim) {
-			anim.SetBool ("animate", true);
-			yield return new WaitForSeconds (getAnimationDuration (anim, "AnimationPillar"));
-			//anim.SetBool ("Trigger", false); // dont reset. should be animated once			
-		}
-		yield return new WaitForSeconds (0.3f);
 	}
-
-	/**
-	 * Duration between the Pillar-Animations endresult and its original position
-	 * @return duration in seconds
-	 */
-	private float getAnimationDuration(Animator a, string n) {
-		foreach (AnimationClip clip in a.runtimeAnimatorController.animationClips)
-			if (clip.name == n) return clip.length;
-		return 0;
-	}
-}
 }
