167cf884b528f6206681ed0d41cdd8d65cfff500
relikd <info@relikd.de>
2017-11-08 17:03:32 +0100

    Hotfix for platform dependent screenshot tool

--- Assets/_custom_Scripts/Generic/ScreenshotTool.cs
+++ Assets/_custom_Scripts/Generic/ScreenshotTool.cs
@@ -6,29 +6,29 @@ using System.IO;
  */ 
 public class ScreenshotTool : MonoBehaviour {
 
-    private int count = 0;
-    private string dir = "Screenshots";
+    // private int count = 0;
+    // private string dir = "Screenshots";
 
     /**
     * Creates the Folder if it doesn't exist and  get the number for the next screenshot.
     */ 
     void Start () {
-        if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
-        DirectoryInfo dirinf = new DirectoryInfo(dir);
-        FileInfo[] info = dirinf.GetFiles("Screenshot*");
-        foreach (FileInfo f in info)
-        {
-            string[] substrings = f.Name.Split('.');
-            int filenumber = int.Parse(substrings[0].Substring(10));
-            if (filenumber >= count) count = filenumber + 1;
-        }
+        // if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+        // DirectoryInfo dirinf = new DirectoryInfo(dir);
+        // FileInfo[] info = dirinf.GetFiles("Screenshot*");
+        // foreach (FileInfo f in info)
+        // {
+        //     string[] substrings = f.Name.Split('.');
+        //     int filenumber = int.Parse(substrings[0].Substring(10));
+        //     if (filenumber >= count) count = filenumber + 1;
+        // }
     }
 
     /**
     * Takes a Screenshot if 'X' is released and increases the count by 1.
     */
     void Update () {
-        if (Input.GetKeyUp(KeyCode.X))       
-            Application.CaptureScreenshot("Screenshots/Screenshot" + count++ + ".png");
-        }
+        // if (Input.GetKeyUp(KeyCode.X))       
+        //     Application.CaptureScreenshot("Screenshots/Screenshot" + count++ + ".png");
+    }
 }
